a1001e502fe73e8ee76a40239eae1175c66ecdda
Brian Yu <brian@byu.io>
2020-04-05 21:47:35 -0400

    Fix merge

--- tcf_website/views/__init__.py
+++ tcf_website/views/__init__.py
@@ -1,11 +1,7 @@
 # See https://docs.djangoproject.com/en/3.0/topics/db/models/#organizing-models-in-a-package
 
 from .auth import login, login_error, logout, collect_extra_info
-<<<<<<< HEAD
-from .browse import browse, index, department
-=======
 from .index import index, about
 from .browse import browse, department, course, course_instructor
 from .review import new_review, reviews
 from .profile import profile
->>>>>>> migration
