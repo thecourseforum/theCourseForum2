2c92502723c20aa1409b0a711ad6dac7045c480c
Jennifer Long <j.alicia.long@gmail.com>
2020-04-15 04:31:26 -0400

    added about page w view context injection into template

--- tcf_website/urls.py
+++ tcf_website/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about', views.about, name='about'),
+    path('about', views.AboutView.as_view(), name='about'),
     path('privacy', views.privacy, name='privacy'),
     path('terms', views.terms, name='terms'),
     path('browse', views.browse, name='browse'),
--- tcf_website/views/__init__.py
+++ tcf_website/views/__init__.py
@@ -4,7 +4,7 @@
 # https://docs.djangoproject.com/en/3.0/topics/db/models/#organizing-models-in-a-package
 
 from .auth import login, login_error, logout, collect_extra_info
-from .index import index, about, privacy, terms
+from .index import index, AboutView, privacy, terms
 from .browse import browse, department, course_view, course_instructor
 from .review import new_review
 from .profile import profile, reviews
