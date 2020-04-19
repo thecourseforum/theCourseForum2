d0db66ee484858b13e457c5086a33eba0dce1bdd
vagrant <vagrant@vagrant.vm>
2020-04-19 19:29:38 +0000

    Adding myself to the about page

--- tcf_website/views/index.py
+++ tcf_website/views/index.py
@@ -113,6 +113,12 @@ class AboutView(TemplateView):
                          "class": "2023",
                          "img_filename": "ENG_Vi_Nguyen.jpg",
                          "github": "vn6"
+                         },
+                        {"name": "Amrit Gorle",
+                         "role": "Developer",
+                         "class": "2023",
+                         "img_filename": "ENG_Amrit_Gorle.jpg",
+                         "github": "amritgorle"
                          }
                         ]
 
