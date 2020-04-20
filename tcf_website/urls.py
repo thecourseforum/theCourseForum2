bd322335c4fc9608f2caf2501e320d43eb21a9a6
Sai Konuri <konurisai2020@gmail.com>
2020-04-20 16:57:16 -0400

    Fix pylint error

--- tcf_website/urls.py
+++ tcf_website/urls.py
@@ -1,10 +1,11 @@
 """Routes URLs to views"""
 
 from django.urls import path
-
-from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+from . import views
+
 
 urlpatterns = [
     path('', views.index, name='index'),
