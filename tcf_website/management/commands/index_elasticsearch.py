a805fbd7093338f6e343228166881f4c4447e293
Brian Yu <brian@byu.io>
2020-04-27 21:25:17 -0400

    Search Results from Elastic (#79) (#86)
    * updated comments
    
    * removed excessive commenting
    
    * Working "Instructors" engine
    
    got a another engine working for the Instructor model
    
    * created blank search landing page
    
    * elastic querying works
    
    * response results now format based on engine type
    
    * added id
    
    * simple search results UI
    
    * forgot to add public Dev settings
    
    * removed small detail
    
    * FIXED BUG, wasn't installing 'requests' library
    
    * added mnemonic to indexer & fetcher
    
    elastic app search
    
    * added mnemonic to Course search results + narrowed css scope
    
    * VERY IMPORTANT Elastic clarification
    
    this should be locked in permenantly
    
    * added documentation
    
    * function name clarification
    
    the new name is more representative of what the function actually does
    
    * removed login requirement for search
    
    we will add it back later but want to discuss whether search and browse will both require login
    
    * fixed "catching too general exception" pylint warning
    
    requests.RequestException catches any non-timeout related error in a request call
    
    * Switched order of name and mnemonic in search output
    
    * Added sorting search results by department alphabetically
    
    Co-authored-by: Brian Yu <brian@byu.io>
    Co-authored-by: Alex Shen <shen.alexander@gmail.com>
    
    Co-authored-by: Bradley Knaysi <30380036+bradknaysi@users.noreply.github.com>
    Co-authored-by: Alex Shen <shen.alexander@gmail.com>

--- tcf_website/management/commands/index_elasticsearch.py
+++ tcf_website/management/commands/index_elasticsearch.py
@@ -1,6 +1,4 @@
-"""
-Modules used
-"""
+"""Management command that loads courses and instructors into Elasticsearch"""
 import os
 import json
 import requests
@@ -36,56 +34,91 @@ class Command(BaseCommand):
 
     """
 
-    help = 'Indexes / Updates the Elastic-hosted cluster'
+    help = 'Indexes the Elastic AppSearch instance w/ Course and Instructor data.'
 
     def handle(self, *args, **options):
 
-        # Set API endpoint
-        courses_engine_endpoint = os.environ['ES_COURSE_ENDPOINT']
-
-        # Get courses
+        courses_engine_endpoint = os.environ['ES_COURSE_DOCUMENTS_ENDPOINT']
+        instructors_engine_endpoint = os.environ['ES_INSTRUCTOR_DOCUMENTS_ENDPOINT']
         all_courses = Course.objects.all().order_by('pk')
+        all_instructors = Instructor.objects.all().order_by('pk')
         self.stdout.write("Number of Courses: " + str(len(all_courses)))
+        self.stdout.write("Number of Instructors: " + str(len(all_instructors)))
 
-        # Prepare parameters
+        batch_size = 100 # MUST NOT EXCEED 100
         documents = []
         count = 0
-        batch_size = 100  # MUST NOT EXCEED 100
+
+        # Debug variables
+        start = 0
+        end = batch_size
+
         for course in all_courses:
 
             document = {
-                "id": course.pk,
-                "title": course.title,
-                "description": course.description,
-                "number": course.number
+                "id" : course.pk,
+                "title" : course.title,
+                "description" : course.description,
+                "number" : course.number,
+                "mnemonic" : course.code()
             }
-
-            # Add document
             documents.append(document)
             count += 1
 
-            # Batching requests for efficiency
+            # Send courses to API in groups
             if count == batch_size:
-
-                # POST to Elastic
                 self.post(documents, courses_engine_endpoint)
-
-                # Prepare next batch
                 count = 0
                 documents.clear()
 
+                # For debug
+                self.stdout.write("Indexed Courses " + str(start) + " - " + str(end))
+                start = end
+                end += batch_size
+
         # Handle remaining documents
         if len(documents) > 0:
-
-            # POST to Elastic
             self.post(documents, courses_engine_endpoint)
+            self.stdout.write("Indexed Courses " + str(start) + " - " + str(start + count))
+
+        # Reset
+        documents.clear()
+        count = 0
+        start = 0
+        end = batch_size
+
+        for instructor in all_instructors:
+
+            document = {
+                "id" : instructor.pk,
+                "first_name" : instructor.first_name,
+                "last_name" : instructor.last_name,
+                "email" : instructor.email,
+                "website" : instructor.website,
+            }
+            documents.append(document)
+            count += 1
+
+            # Send instructors to API in groups
+            if count == batch_size:
+                self.post(documents, instructors_engine_endpoint)
+                count = 0
+                documents.clear()
+
+                # For debug
+                self.stdout.write("Indexed Instructors " + str(start) + " - " + str(end))
+                start = end
+                end += batch_size
+
+        # Handle remaining documents
+        if len(documents) > 0:
+            self.post(documents, instructors_engine_endpoint)
+            self.stdout.write("Indexed Instructors " + str(start) + " - " + str(start + count))
 
     def post(self, documents, api_endpoint):
-        """Posts documents to a Document API endpoint
-        """
+        """Posts documents to a Document API endpoint"""
 
-        # Same for all engines
-        api_key = os.environ['ES_API_KEY']
+        api_key = os.environ['ES_PRIVATE_API_KEY']
         https_headers = {
             "Content-Type": "application/json",
             "Authorization": "Bearer " + api_key
@@ -95,13 +128,13 @@ class Command(BaseCommand):
         json_documents = json.dumps(documents)
 
         try:
-            # Send documents to the Elastic endpoint
             response = requests.post(
                 url=api_endpoint,
                 data=json_documents,
                 headers=https_headers
             )
-            self.stdout.write("status_code = " + str(response.status_code))
+            if response.status_code != 200:
+                raise CommandError("status_code = " + str(response.status_code) + " -- " + str(response.text))
 
         except Exception as error:
-            raise CommandError("Elastic indexing error: " + str(error))
+            raise CommandError("Error: " + str(error))
---
+++ tcf_website/views/search.py
@@ -0,0 +1,108 @@
+"""Views for search results"""
+import os
+import json
+import requests
+
+from django.shortcuts import render
+#from django.contrib.auth.decorators import login_required
+
+def search(request):
+    """Search results view."""
+
+    # Set query
+    query = request.GET.get('q', '')
+
+    # Set endpoints
+    courses_search_endpoint = os.environ['ES_COURSE_SEARCH_ENDPOINT']
+    instructors_search_endpoint = os.environ['ES_INSTRUCTOR_SEARCH_ENDPOINT']
+
+    # Fetch results
+    response1 = fetch_elasticsearch(query, courses_search_endpoint)
+    response2 = fetch_elasticsearch(query, instructors_search_endpoint)
+
+    # Format results
+    courses = format_response(response1)
+    instructors = format_response(response2)
+
+    # Arguments for template
+    args = {
+        "courses" : courses,
+        "instructors" : instructors,
+        "query" : query
+    }
+
+    return render(request, 'search/search.html', args)
+
+
+def fetch_elasticsearch(query, api_endpoint):
+    """Fetches documents based on a query from a Document API endpoint"""
+
+    api_key = os.environ['ES_PUBLIC_API_KEY']
+    https_headers = {
+        "Content-Type" : "application/json",
+        "Authorization" : "Bearer " + api_key
+    }
+
+    # Format API query string
+    json_query = json.dumps({"query": query})
+
+    try:
+        response = requests.get(
+            url=api_endpoint,
+            headers=https_headers,
+            data=json_query
+        )
+        return response
+
+    except requests.RequestException as error:
+        return "Error: " + str(error)
+
+def format_response(response):
+    """Formats an Elastic search endpoint response"""
+
+    body = json.loads(response.text)
+    engine = body.get("meta").get("engine").get("name")
+    results = body.get("results")
+
+    if engine == "uva-courses":
+        return format_courses(results)
+    if engine == "uva-instructors":
+        return format_instructors(results)
+
+    return "Unknown engine, please verify engine exists"
+
+def format_courses(results):
+    """Formats courses engine results"""
+
+    formatted = []
+    for result in results:
+
+        course = {
+            "id" : result.get("_meta").get("id"),
+            "title" : result.get("title").get("raw"),
+            "description" : result.get("description").get("raw"),
+            "number" : result.get("number").get("raw"),
+            "mnemonic" : result.get("mnemonic").get("raw")
+        }
+        formatted.append(course)
+
+    formatted.sort(key=lambda x: x["mnemonic"][0:x["mnemonic"].index(" ")])
+
+    return formatted
+
+def format_instructors(results):
+    """Formats instructors engine results"""
+
+    formatted = []
+    for result in results:
+
+        instructor = {
+            "id" : result.get("_meta").get("id"),
+            "first_name" : result.get("first_name").get("raw"),
+            "last_name" : result.get("last_name").get("raw"),
+            "email" : result.get("email").get("raw"),
+            "website" : result.get("website").get("raw")
+        }
+        formatted.append(instructor)
+
+    return formatted
