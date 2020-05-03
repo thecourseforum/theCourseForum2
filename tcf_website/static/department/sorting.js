13386cd1350128fb2933ce422565504dae40d580
jackliu612 <jackliu612@gmail.com>
2020-05-02 21:09:33 -0400

    Added course sorting to the departments page

--- tcf_website/static/department/sorting.js
+++ tcf_website/static/department/sorting.js
@@ -1,4 +1,85 @@
-const sort = (cmp) => {
+let number_A = true;
+let rating_A = false;
+let difficulty_A = true;
+let gpa_A = false;
+
+/*
+    Each method sort_by_... is the same format as outline below
+    sort_classes takes in a comparing function (defined below) and reorders the classes appropriately
+    comparator needed for both ascending and decending as any classes with null values should be at the end of the list either order
+*/
+const sort_by_number = () => {
+    // Updating looks and sorting order
+    if (!$('#number-sort-btn').hasClass('active')) {
+        $('#rating-sort-btn').removeClass('active')
+        $('#diff-sort-btn').removeClass('active')
+        $('#gpa-sort-btn').removeClass('active')
+        $('#number-sort-btn').addClass('active')
+    } else {               // Already selected so simply reverse the order of sorting (ascending vs descending)
+        number_A = !number_A;
+    }
+
+    // Actual sorting
+    if (number_A) {
+        sort_classes(cmp_by_number_A)
+    } else {
+        sort_classes(cmp_by_number_D)
+    }
+}
+
+const sort_by_rating = () => {
+    if (!$('#rating-sort-btn').hasClass('active')) {
+        $('#number-sort-btn').removeClass('active')
+        $('#diff-sort-btn').removeClass('active')
+        $('#gpa-sort-btn').removeClass('active')
+        $('#rating-sort-btn').addClass('active')
+    } else {
+        rating_A = !rating_A;
+    }
+
+    if (rating_A) {
+        sort_classes(cmp_by_rating_A)
+    } else {
+        sort_classes(cmp_by_rating_D)
+    }
+}
+
+const sort_by_difficulty = () => {
+    if (!$('#diff-sort-btn').hasClass('active')) {
+        $('#number-sort-btn').removeClass('active')
+        $('#rating-sort-btn').removeClass('active')
+        $('#gpa-sort-btn').removeClass('active')
+        $('#diff-sort-btn').addClass('active')
+    } else {
+        difficulty_A = !difficulty_A;
+    }
+
+    if (difficulty_A) {
+        sort_classes(cmp_by_difficulty_A)
+    } else {
+        sort_classes(cmp_by_difficulty_D)
+    }
+}
+
+const sort_by_gpa = () => {
+    if (!$('#gpa-sort-btn').hasClass('active')) {
+        $('#number-sort-btn').removeClass('active')
+        $('#rating-sort-btn').removeClass('active')
+        $('#diff-sort-btn').removeClass('active')
+        $('#gpa-sort-btn').addClass('active')
+    } else {
+        gpa_A = !gpa_A;
+    }
+
+    if (gpa_A) {
+        sort_classes(cmp_by_gpa_A)
+    } else {
+        sort_classes(cmp_by_gpa_D)
+    }
+}
+
+
+const sort_classes = (cmp) => {
     $("ul.course-list").each(function (a) {
         var li = $(this).children("li");
         li.detach().sort(cmp);
@@ -6,12 +87,14 @@ const sort = (cmp) => {
     });
 }
 
-const sort_by_rating_D = (a, b) => {
+const cmp_by_rating_D = (a, b) => {
     ratingA = a.querySelectorAll(".mb-0.info")[0].innerHTML.trim();
     ratingB = b.querySelectorAll(".mb-0.info")[0].innerHTML.trim();
 
-    if (ratingA === '—' | ratingB === '—') {
+    if (ratingA === '—') {
         return 1;
+    } else if (ratingB === '—') {
+        return -1;
     }
 
     if (ratingA > ratingB) {
@@ -23,12 +106,14 @@ const sort_by_rating_D = (a, b) => {
     }
 };
 
-const sort_by_rating_A = (a, b) => {
+const cmp_by_rating_A = (a, b) => {
     ratingA = a.querySelectorAll(".mb-0.info")[0].innerHTML.trim();
     ratingB = b.querySelectorAll(".mb-0.info")[0].innerHTML.trim();
 
-    if (ratingA === '—' | ratingB === '—') {
+    if (ratingA === '—') {
         return 1;
+    } else if (ratingB === '—') {
+        return -1;
     }
 
     if (ratingA > ratingB) {
@@ -40,12 +125,14 @@ const sort_by_rating_A = (a, b) => {
     }
 };
 
-const sort_by_difficulty_D = (a, b) => {
+const cmp_by_difficulty_D = (a, b) => {
     difficultyA = a.querySelectorAll(".mb-0.info")[1].innerHTML.trim();
     difficultyB = b.querySelectorAll(".mb-0.info")[1].innerHTML.trim();
 
-    if (difficultyA === '—' | difficultyB === '—') {
+    if (difficultyA === '—') {
         return 1;
+    } else if (difficultyB === '—') {
+        return -1;
     }
 
     if (difficultyA > difficultyB) {
@@ -57,12 +144,14 @@ const sort_by_difficulty_D = (a, b) => {
     }
 };
 
-const sort_by_difficulty_A = (a, b) => {
+const cmp_by_difficulty_A = (a, b) => {
     difficultyA = a.querySelectorAll(".mb-0.info")[1].innerHTML.trim();
     difficultyB = b.querySelectorAll(".mb-0.info")[1].innerHTML.trim();
 
-    if (difficultyA === '—' | difficultyB === '—') {
+    if (difficultyA === '—') {
         return 1;
+    } else if (difficultyB === '—') {
+        return -1;
     }
 
     if (difficultyA > difficultyB) {
@@ -74,7 +163,7 @@ const sort_by_difficulty_A = (a, b) => {
     }
 };
 
-const sort_by_gpa_D = (a, b) => {
+const cmp_by_gpa_D = (a, b) => {
     gpaA = a.querySelectorAll(".mb-0.info")[2].innerHTML.trim();
     gpaB = b.querySelectorAll(".mb-0.info")[2].innerHTML.trim();
 
@@ -87,7 +176,7 @@ const sort_by_gpa_D = (a, b) => {
     }
 };
 
-const sort_by_gpa_A = (a, b) => {
+const cmp_by_gpa_A = (a, b) => {
     gpaA = a.querySelectorAll(".mb-0.info")[2].innerHTML.trim();
     gpaB = b.querySelectorAll(".mb-0.info")[2].innerHTML.trim();
 
@@ -100,7 +189,7 @@ const sort_by_gpa_A = (a, b) => {
     }
 };
 
-const sort_by_number_D = (a, b) => {
+const cmp_by_number_D = (a, b) => {
     numberA = a.getElementsByTagName("h3")[0].innerHTML;
     numberB = b.getElementsByTagName("h3")[0].innerHTML;
 
@@ -113,7 +202,7 @@ const sort_by_number_D = (a, b) => {
     }
 };
 
-const sort_by_number_A = (a, b) => {
+const cmp_by_number_A = (a, b) => {
     numberA = a.getElementsByTagName("h3")[0].innerHTML;
     numberB = b.getElementsByTagName("h3")[0].innerHTML;
 
