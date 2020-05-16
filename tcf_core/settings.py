a9339204cbf0b0e8bfd376cd4f38e16236005f2d
Jennifer Long <j.alicia.long@gmail.com>
2020-05-15 20:11:46 -0400

    readd dj-environ

--- tcf_core/settings.py
+++ tcf_core/settings.py
@@ -12,10 +12,14 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+env = environ.Env(DEBUG=(bool, False))
+env_file = os.path.join(BASE_DIR, ".env")
+environ.Env.read_env(env_file)
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
