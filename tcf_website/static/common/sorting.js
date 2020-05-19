acffa1fef7c84b4973b807333b956f5a708b3485
jackliu612 <jackliu612@gmail.com>
2020-05-19 10:31:12 -0400

    Fixed formatting issues

--- tcf_website/static/common/sorting.js
+++ tcf_website/static/common/sorting.js
@@ -4,46 +4,46 @@
 
 // prop is html id of property to be sorted by
 // asc = 1 for ascending, asc = -1 for descending
-function sortHTML(container, items, prop, asc=1){
-    cmp = cmpByProp(prop, asc);
-    var objs = $(items);
-    objs.detach().sort(cmp);
-    $(container).append(objs);
+function sortHTML(container, items, prop, asc = 1) {
+  cmp = cmpByProp(prop, asc);
+  var objs = $(items);
+  objs.detach().sort(cmp);
+  $(container).append(objs);
 }
 
 function cmpByProp(prop, asc) {
-    return function(a, b) {
-      id = '#'.concat(prop)
-      valA = a.querySelector(id).innerHTML.trim();
-      valB = b.querySelector(id).innerHTML.trim();
+  return function (a, b) {
+    id = '#'.concat(prop);
+    valA = a.querySelector(id).innerHTML.trim();
+    valB = b.querySelector(id).innerHTML.trim();
 
-      if(prop=='date'){ //convert date string to int
-        valA = dateToInt(valA);
-        valB = dateToInt(valB);
-      }else{ //extract number from string
-        try {
-          valA = valA.match(/[\d\.]+/)[0];
-        } catch(err) {
-          return 1;
-        }
-        try {
-          valB = valB.match(/[\d\.]+/)[0];
-        } catch(err) {
-          return -1;
-        }
+    if (prop == 'date') { //convert date string to int
+      valA = dateToInt(valA);
+      valB = dateToInt(valB);
+    } else { //extract number from string
+      try {
+        valA = valA.match(/[\d\.]+/)[0];
+      } catch (err) {
+        return 1;
       }
-
-      if (valA > valB) {
-        return asc;
-      } else if (valA < valB) {
-        return -1*asc;
-      } else {
-        return 0;
+      try {
+        valB = valB.match(/[\d\.]+/)[0];
+      } catch (err) {
+        return -1;
       }
     }
+
+    if (valA > valB) {
+      return asc;
+    } else if (valA < valB) {
+      return -1 * asc;
+    } else {
+      return 0;
+    }
+  }
 }
 
-function dateToInt(date_str){
+function dateToInt(date_str) {
   date = new Date(date_str);
   return date.getTime();
 }
