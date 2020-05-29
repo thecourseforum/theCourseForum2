09dc6402aa92e06e21d6e71173ebd5c08b06c6ba
Bradley Knaysi <bak9cu@virginia.edu>
2020-05-29 00:46:04 -0400

    added empty javascript file

---
+++ tcf_website/static/about/javascript/switch-subcontent.js
@@ -0,0 +1,8 @@
+/*
+    This javascript is used to select between
+    history, current team, and past contributors
+    when loading the about page subcontent on theCourseForum.
+
+    Author: Bradley Knaysi
+*/
+
