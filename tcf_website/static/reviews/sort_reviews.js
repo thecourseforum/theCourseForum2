46baa98fbbd7c3680c911d0897c7945158c58700
Vi Nguyen <34071587+vn6@users.noreply.github.com>
2020-06-04 12:58:18 -0400

    Course and review sorting with common sorting file (#120)
    * added review sorting with common sorting file
    
    * Updated common sorting code and added to courses
    
    * moved dateToInt function
    
    * Fixed formatting issues
    
    * updated .gitignore
    
    Co-authored-by: jackliu612 <jackliu612@gmail.com>

---
+++ tcf_website/static/reviews/sort_reviews.js
@@ -0,0 +1,42 @@
+function makeActive(htmlTag, label){
+  $('.dropdown-item').removeClass('active');
+  $(htmlTag).addClass('active');
+  $('#review-sort-select').html(label);
+}
+
+function sortReviews(btnID){
+  htmlTag = '#'.concat(btnID);
+  label = "";
+  prop = "";
+  asc = -1;
+  switch (btnID) {
+    case "votes-sort-btn":
+      label = 'Most Helpful';
+      prop = 'vote-count';
+      break;
+    case "recent-sort-btn":
+      label = 'Most Recent';
+      prop = 'date';
+      break;
+    case "highrating-sort-btn":
+      label = 'Highest Rating';
+      prop = 'review-average';
+      break;
+    case "lowrating-sort-btn":
+      label = 'Lowest Rating';
+      prop = 'review-average';
+      asc = 1;
+      break;
+    default:
+      console.log("error");
+      return;
+  }
+  if (!$(htmlTag).hasClass('active')) {
+    makeActive(htmlTag, label);
+    sortHTML('.reviews', '.review', prop, asc);
+  }
+}
+
+$(window).on('load', function() {
+  sortReviews("votes-sort-btn");
+});
