262ed4fcfa1a0481cf8428bd61f2161e997e25d0
Bradley Knaysi <30380036+bradknaysi@users.noreply.github.com>
2020-06-13 16:16:13 -0400

    Better "About Page" (#137)
    * about header section done
    
    * about support section done
    
    * tweaks + code cleanup
    
    * removed old html
    
    * fixed icon formatting
    
    I referred to the actual fontawesome documentation and formatted them correctly.
    
    * about nav section done
    
    * polished size symmetry
    
    * consolidated @media css
    
    cleaner code, same functionality
    
    * renamed css
    
    * improved css readability
    
    * removed outer container
    
    The container was adding padding that I didn't want. Also now the html is simplier!
    
    * added empty javascript file
    
    * working about page (still need to polish subcontent)
    
    * polished css (mostly "tabs")
    
    * improved PR readability
    
    * history page done
    
    * about page tweaks
    
    * renamed + moved sponsors photos
    
    * removed old URLs
    
    * replaced any old logos with the new logo
    
    * refactored history.css to be less redundant
    
    * added tagline :100:

--- tcf_website/views/__init__.py
+++ tcf_website/views/__init__.py
@@ -4,7 +4,7 @@
 # https://docs.djangoproject.com/en/3.0/topics/db/models/#organizing-models-in-a-package
 
 from .auth import login, login_error, logout, collect_extra_info
-from .index import index, AboutView, AboutHistoryView, AboutContributorsView, privacy, terms
+from .index import index, AboutView, privacy, terms
 from .browse import browse, department, course_view, course_instructor
 from .review import new_review, upvote, downvote
 from .profile import profile, reviews
