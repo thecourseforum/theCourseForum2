f0a9ba06149e984293bec7323b9dad5152fe5d4a
jackliu612 <jackliu612@gmail.com>
2020-10-17 19:02:27 -0400

    Added the moving of CS to ESchool to management command

--- tcf_website/management/commands/other_schools_department_fix.py
+++ tcf_website/management/commands/other_schools_department_fix.py
@@ -3,17 +3,33 @@ from tcf_website.models import *
 
 
 class Command(BaseCommand):
+
     help = 'Promotes subdepartments in \'Other Schools at the University of Virginia\' to departments'
 
+    def add_arguments(self, parser):
+        # Named (optional) arguments
+        parser.add_argument(
+            '--verbose',
+            action='store_true',
+            help='Verbose output',
+        )
+
     def handle(self, *args, **options):
+
+        self.verbose = options['verbose']
+
+        # Take Other Schools at UVA and promote all subdepartments into departments
+        # Excluded schoosl not to split up
         excluded = [
             "College of Arts & Sciences",
             "School of Engineering & Applied Science",
             "UNKNOWN"]
-        # dash = '-'*185
-        # print(dash)
-        # print('{:<70s}{:<70s}{:<70s}'.format('Subdepartment', 'Department', 'School'))
-        # print(dash)
+
+        if self.verbose:
+            dash = '-'*185
+            print(dash)
+            print('{:<70s}{:<70s}{:<70s}'.format('Subdepartment', 'Department', 'School'))
+            print(dash)
 
         # Go through all schools that are not in the list of excluded
         for school in School.objects.exclude(name__in=excluded):
@@ -21,8 +37,9 @@ class Command(BaseCommand):
             for department in school.department_set.all():
                 # Go through all subdepartments
                 for subdepartment in department.subdepartment_set.all():
-                    #             print('{:<70s}{:<70s}{:<70s}'.format(subdepartment.name, department.name, school.name))
-                    #     print(dash)   
+                    if self.verbose:
+                        print('{:<70s}{:<70s}{:<70s}'.format(subdepartment.name, department.name, school.name))
+                        print(dash)   
                     # Promote the subdepartment to a department
                     new_department = Department(
                         name=subdepartment.name,
@@ -35,3 +52,16 @@ class Command(BaseCommand):
                     subdepartment.save()
                 # Delete old department
                 department.delete()
+
+        ## Moving Computer Science from A&S to School of Engineering
+        # Get the School of Engineering and Computer Science department instances
+        E_School = School.objects.get(name='School of Engineering & Applied Science')
+        Comp_Sci = Department.objects.get(name="Computer Science")
+        # Assign school
+        Comp_Sci.school = E_School
+        # Save changes
+        Comp_Sci.save()
+        
+        if self.verbose:
+            print('Moved Computer Science to School of Engineering')
+
