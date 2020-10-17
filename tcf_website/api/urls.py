60e190a1d4c53690b61d67bfac1cf1af1fa5031b
Jihoon Park <bartleby2718@gmail.com>
2020-10-18 01:26:23 +0900

    Add Department API

--- tcf_website/api/urls.py
+++ tcf_website/api/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register(r'schools', views.SchoolViewSet)
+router.register(r'departments', views.DepartmentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
