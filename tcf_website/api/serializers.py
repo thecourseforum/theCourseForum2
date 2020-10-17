992ad77faf7796b73af5a932f04fcba4e8e6a890
Jihoon Park <bartleby2718@gmail.com>
2020-10-18 03:22:12 +0900

    Add paginated Instructor API and filter by Course ID

--- tcf_website/api/serializers.py
+++ tcf_website/api/serializers.py
@@ -1,6 +1,6 @@
 """DRF Serializers"""
 from rest_framework import serializers
-from ..models import Course, Department, School, Subdepartment
+from ..models import Course, Department, School, Instructor, Subdepartment
 
 
 class SchoolSerializer(serializers.ModelSerializer):
@@ -30,4 +30,11 @@ class CourseSerializer(serializers.ModelSerializer):
     """DRF Serializer for Course"""
     class Meta:
         model = Course
+        fields = '__all__'
+
+
+class InstructorSerializer(serializers.ModelSerializer):
+    """DRF Serializer for Instructor"""
+    class Meta:
+        model = Instructor
         fields = '__all__'
