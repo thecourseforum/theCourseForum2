82d0dc1c41895880041675981d2eabfcc1141a42
julesfll <jfl4kb@virginia.edu>
2020-10-17 18:35:25 -0400

    Added basic form UI

--- tcf_website/views/profile.py
+++ tcf_website/views/profile.py
@@ -2,12 +2,27 @@
 
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django import forms
+import logging
+logger = logging.getLogger(__name__)
+
+class ProfileForm(forms.Form):
+    first_name = forms.CharField(label='First Name', max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(label='Last Name', max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    graduation_year = forms.IntegerField(label='Graduation Year', widget=forms.NumberInput(attrs={'class': 'form-control'}))
 
 
 @login_required
 def profile(request):
     """User profile view."""
-    return render(request, 'profile/profile.html')
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, label_suffix='')
+
+        if form.is_valid():
+            logger.error(form.cleaned_data['test_field'])
+    else:
+        form = ProfileForm(label_suffix='')
+    return render(request, 'profile/profile.html', {'form': form})
 
 
 @login_required
