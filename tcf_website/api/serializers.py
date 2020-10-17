3fa5bb71e012bd52f731b0b86583c34f28447344
Jihoon Park <bartleby2718@gmail.com>
2020-10-18 01:26:23 +0900

    Add Department API

--- tcf_website/api/serializers.py
+++ tcf_website/api/serializers.py
@@ -1,10 +1,19 @@
 """DRF Serializers"""
 from rest_framework import serializers
-from ..models import School
+from ..models import Department, School
 
 
 class SchoolSerializer(serializers.ModelSerializer):
     """DRF Serializer for School"""
     class Meta:
         model = School
+        fields = '__all__'
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    """DRF Serializer for Department"""
+    school = SchoolSerializer(read_only=True)
+
+    class Meta:
+        model = Department
         fields = '__all__'
--- tcf_website/api/views.py
+++ tcf_website/api/views.py
@@ -1,11 +1,17 @@
 # pylint: disable=too-many-ancestors
 """DRF Viewsets"""
 from rest_framework import viewsets
-from ..models import School
-from .serializers import SchoolSerializer
+from ..models import Department, School
+from .serializers import DepartmentSerializer, SchoolSerializer
 
 
 class SchoolViewSet(viewsets.ReadOnlyModelViewSet):
     """DRF ViewSet for School"""
     queryset = School.objects.all()
     serializer_class = SchoolSerializer
+
+
+class DepartmentViewSet(viewsets.ReadOnlyModelViewSet):
+    """DRF ViewSet for Department"""
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
