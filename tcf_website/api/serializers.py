7d823cdf652ae4f87aef6d5836afd8ce4ff80b42
Jihoon Park <bartleby2718@gmail.com>
2020-10-18 03:35:20 +0900

    Add Semester API (just in case)

--- tcf_website/api/serializers.py
+++ tcf_website/api/serializers.py
@@ -1,6 +1,7 @@
 """DRF Serializers"""
 from rest_framework import serializers
-from ..models import Course, Department, School, Instructor, Subdepartment
+from ..models import (Course, Department, School, Instructor, Semester,
+                      Subdepartment)
 
 
 class SchoolSerializer(serializers.ModelSerializer):
@@ -37,4 +38,11 @@ class InstructorSerializer(serializers.ModelSerializer):
     """DRF Serializer for Instructor"""
     class Meta:
         model = Instructor
+        fields = '__all__'
+
+
+class SemesterSerializer(serializers.ModelSerializer):
+    """DRF Serializer for Semester"""
+    class Meta:
+        model = Semester
         fields = '__all__'
