cc670ea512a3ebce43080123615cda38d70bdb71
Alex Shen <shen.alexander@gmail.com>
2020-10-25 18:08:38 -0400

    Create instructor page views/template

--- tcf_website/views/__init__.py
+++ tcf_website/views/__init__.py
@@ -5,7 +5,7 @@
 
 from .auth import login, login_error, logout, collect_extra_info
 from .index import index, AboutView, AboutHistoryView, AboutContributorsView, privacy, terms
-from .browse import browse, department, course_view, course_instructor
+from .browse import browse, department, course_view, course_instructor, instructor_view
 from .review import new_review, upvote, downvote
 from .profile import profile, reviews
 from .search import search
