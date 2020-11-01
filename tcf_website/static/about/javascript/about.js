f97ca22ea546e1da5d1fb826d9983e3d34085b8a
julesfll <juleslemenestrel@gmail.com>
2020-11-01 12:28:53 -0500

    Add sponsors tab

--- tcf_website/static/about/javascript/about.js
+++ tcf_website/static/about/javascript/about.js
@@ -1,5 +1,6 @@
-import { showHistory, showCurrentTeam, showPastContributors } from "../javascript/switch-subcontent.js";
+import { showHistory, showCurrentTeam, showPastContributors, showSponsors } from "../javascript/switch-subcontent.js";
 
 document.getElementById('tCF-history-btn').addEventListener('click', showHistory, false);
 document.getElementById('tCF-current-team-btn').addEventListener('click', showCurrentTeam, false);
 document.getElementById('tCF-past-contributors-btn').addEventListener('click', showPastContributors, false);
+document.getElementById('tCF-sponsors-btn').addEventListener('click', showSponsors, false);
