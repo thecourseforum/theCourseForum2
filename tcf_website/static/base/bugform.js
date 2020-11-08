6696623fe1fd17dd721aaba7a758780f07d234a2
vn6 <34071587+vn6@users.noreply.github.com>
2020-11-08 14:43:27 -0500

    eslint build fix

--- tcf_website/static/base/bugform.js
+++ tcf_website/static/base/bugform.js
@@ -1,36 +1,36 @@
 import { validateForm } from "../common/form.js";
 
-function submitForm(){
-  var form = document.getElementById("bugform");
-  var valid = validateForm(form);
-  if (valid === true) {
-    postToDiscord();
-    // close form after submit
-    $('#bugModal').modal('toggle');
-    $('#confirmationModal').modal('toggle');
-  }
+function submitForm() {
+    var form = document.getElementById("bugform");
+    var valid = validateForm(form);
+    if (valid === true) {
+        postToDiscord();
+        // close form after submit
+        $("#bugModal").modal("toggle");
+        $("#confirmationModal").modal("toggle");
+    }
 }
 
-function resetForm(){
-  var form = document.getElementById("bugform");
-  form.classList.remove('was-validated');
-  var emailField = document.getElementById("emailField");
-  emailField.value = "";
-  var descriptionField = document.getElementById("descriptionField");
-  descriptionField.value = "";
+function resetForm() {
+    var form = document.getElementById("bugform");
+    form.classList.remove("was-validated");
+    var emailField = document.getElementById("emailField");
+    emailField.value = "";
+    var descriptionField = document.getElementById("descriptionField");
+    descriptionField.value = "";
 
-  for(var i=1; i<=4; i++){
-    var id = "#category" + i
-    $(id).prop("checked", false);
-  }
+    for(var i = 1; i <= 4; i++) {
+        var id = "#category" + i
+        $(id).prop("checked", false);
+    }
 }
 
-function postToDiscord(){
+function postToDiscord() {
   var url = window.location.href;
   var email = $("#emailField").val();
   var description = $("#descriptionField").val();
   var categories = ""
-  for(var i=1; i<=4; i++){
+  for(var i=1; i<=4; i++) {
     var id = "#category" + i
     if($(id).is(':checked')){
       categories += "[" + $(id).val() + "]"
@@ -40,13 +40,13 @@ function postToDiscord(){
     'content': "Bug Found! \n**URL:** " + url +
                 "\n**Description**: \n" + description +
                 "\n**Categories: **"+ categories  +
-                "\n**Email:** " + email,
+                "\n**Email:** " + email
   }
 
   $.ajax({
         type: "GET",
         url: "/discord/",
-        data: content,
+        data: content
       });
 }
 
