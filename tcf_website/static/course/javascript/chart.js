078f4fc1e62eecb2e789024823bb99adfa0a06ed
Arjun Kumar <arjunkumar01@gmail.com>
2020-11-14 18:38:39 -0500

    fixed some chart responsiveness issues

--- tcf_website/static/course/javascript/chart.js
+++ tcf_website/static/course/javascript/chart.js
@@ -6,17 +6,17 @@ let loadData = async url => {
     } else {
       const { a_plus, a, a_minus, b_plus, b, b_minus, c_plus, c, c_minus, d_plus, d, d_minus, f, withdraw, drop } = data
       const grades_data = [a_plus, a, a_minus, b_plus, b, b_minus, c_plus, c, c_minus, d_plus, d, d_minus, f, withdraw, drop]
-
+ 
       createChart(grades_data)
-
+ 
       const reducer = (accumulator, currentValue) => accumulator + currentValue;
-
+ 
       document.getElementsByClassName("gpa-text")[0].innerHTML = data.average_gpa.toFixed(2) + " GPA"
       document.getElementsByClassName("students-text")[0].innerHTML = grades_data.reduce(reducer) + " Students"
-
+ 
       document.getElementsByClassName("rating_num")[0].innerHTML = data.average_rating.toFixed(2)
       document.getElementsByClassName("hours_num")[0].innerHTML = data.average_hours_per_week.toFixed(2)
-
+ 
       document.getElementsByClassName("professor-num")[0].innerHTML = data.average_instructor.toFixed(2)
       document.getElementsByClassName("fun-num")[0].innerHTML = data.average_fun.toFixed(2)
       document.getElementsByClassName("difficulty-num")[0].innerHTML = data.average_difficulty.toFixed(2)
@@ -25,7 +25,7 @@ let loadData = async url => {
       document.getElementsByClassName("writing-num")[0].innerHTML = data.average_amount_writing.toFixed(2)
       document.getElementsByClassName("group-num")[0].innerHTML = data.average_amount_group.toFixed(2)
       document.getElementsByClassName("homework-num")[0].innerHTML = data.average_amount_homework.toFixed(2)
-
+ 
       document.getElementsByClassName("professor-bar")[0].style.width = 100 * data.average_instructor / 5 + "%"
       document.getElementsByClassName("fun-bar")[0].style.width = 100 * data.average_fun / 5 + "%"
       document.getElementsByClassName("difficulty-bar")[0].style.width = 100 * data.average_difficulty / 5 + "%"
@@ -37,7 +37,7 @@ let loadData = async url => {
     }
   }
 }
-
+ 
 let createChart = grades_data => {
   let chart_data = {
     datasets: [{
@@ -64,13 +64,14 @@ let createChart = grades_data => {
       "A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "D-", "F", "Withdraw", "Drop"
     ]
   }
-  var ctx2 = document.getElementById("myChart2");
-  var myChart2 = new Chart(ctx2, {
+  var ctx = document.getElementById("myChart");
+  var myChart = new Chart(ctx, {
     type: 'pie',
     data: chart_data,
     options: {
       cutoutPercentage: 65,
-      responsive: false,
+      responsive: true,
+      aspectRatio: 1,
       legend: {
         display: false
       },
@@ -78,29 +79,29 @@ let createChart = grades_data => {
         labels: {
           // render 'label', 'value', 'percentage', 'image' or custom function, default is 'percentage'
           render: 'label',
-
+ 
           // font size, default is defaultFontSize
-          fontSize: 14,
-
+          fontSize: 12,
+ 
           // font color, can be color array for each data or function for dynamic color, default is defaultFontColor
           fontColor: '#fff',
-
+ 
           // font style, default is defaultFontStyle
           fontStyle: 'normal',
-
+ 
           // draw label in arc, default is false
           // bar chart ignores this
-          arc: true,
-
+          arc: false,
+ 
           // position to draw label, available value is 'default', 'border' and 'outside'
           // bar chart ignores this
           // default is 'default'
           position: 'default',
-
+ 
           // draw label even it's overlap, default is true
           // bar chart ignores this
           overlap: false,
-
+ 
           // add margin of text when position is `outside` or `border`
           // default is 2
           textMargin: 4
@@ -109,5 +110,5 @@ let createChart = grades_data => {
     }
   });
 }
-
+ 
 export { loadData }
