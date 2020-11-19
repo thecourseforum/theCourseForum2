b422bcdd2aa145c6db588501dad2f667a3033e24
Jihoon Park <bartleby2718@gmail.com>
2020-11-20 01:42:02 +0900

    Improve the performance of reviews without client-side rendering

--- tcf_website/models/models.py
+++ tcf_website/models/models.py
@@ -129,7 +129,17 @@ class User(AbstractUser):
 
     def reviews(self):
         """Return user reviews sorted by creation date."""
-        return self.review_set.order_by("-created")
+        return self.annotate(
+            sum_votes=models.functions.Coalesce(
+                models.Sum('vote__value'),
+                models.Value(0)
+            ),
+            user_vote=models.functions.Coalesce(
+                models.Sum('vote__value',
+                           filter=models.Q(vote__user=self)),
+                models.Value(0)
+            )
+        ).order_by("-created")
 
 
 class Instructor(models.Model):
@@ -690,23 +700,24 @@ class Review(models.Model):
     @staticmethod
     def display_reviews(course, instructor, user):
         """Prepare review list for course-instructor page."""
-
         reviews = Review.objects.filter(
             instructor=instructor,
             course=course,
-        ).exclude(text="").order_by("-created")
-
+        ).exclude(text="").annotate(
+            sum_votes=models.functions.Coalesce(
+                models.Sum('vote__value'),
+                models.Value(0)
+            ),
+        )
         if user.is_authenticated:
-            for review in reviews:
-                try:
-                    review.user_vote = Vote.objects.get(
-                        user=user,
-                        review=review,
-                    ).value
-                except Vote.DoesNotExist:
-                    review.user_vote = 0
-
-        return reviews
+            reviews = reviews.annotate(
+                user_vote=models.functions.Coalesce(
+                    models.Sum('vote__value',
+                               filter=models.Q(vote__user=user)),
+                    models.Value(0)
+                ),
+            )
+        return reviews.order_by("-created")
 
     def __str__(self):
         return f"Review by {self.user} for {self.course} taught by {self.instructor}"
