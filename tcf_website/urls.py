549a4dc89ba94107b43255a02b7a7f8c6664b5c4
Jihoon Park <bartleby2718@gmail.com>
2020-11-20 04:44:59 +0900

    Fix #245 URL gets weird after login

--- tcf_website/urls.py
+++ tcf_website/urls.py
@@ -32,7 +32,6 @@ urlpatterns = [
     path('discord/', views.post_message, name='discord'),
 
     # AUTH URLS
-    path('accounts/profile/', views.browse),
     path('login', views.login, name='login'),
     path('login/error', views.login_error),
     path('login/collect_extra_info', views.collect_extra_info),
