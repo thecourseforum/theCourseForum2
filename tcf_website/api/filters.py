c6b9ba8601e5ebe55ecd59a2c5e086f6a1c4cb3b
Jihoon Park <bartleby2718@gmail.com>
2020-11-23 07:34:37 +0900

    Fix #254 Add recency param to Instructor API

--- tcf_website/api/filters.py
+++ tcf_website/api/filters.py
@@ -1,12 +1,20 @@
 """"Custom DRF pagination classes"""
-from django_filters import FilterSet, NumberFilter
-from ..models import Instructor
+from django_filters import FilterSet, BooleanFilter, NumberFilter
+from ..models import Instructor, Semester
 
 
 class InstructorFilter(FilterSet):
     """Filter for Instructor"""
     section__course = NumberFilter(field_name='section__course', distinct=True)
+    recent5years = BooleanFilter(method='filter_recent5years', distinct=True)
+
+    def filter_recent5years(self, queryset, name, value):
+        if value is True:
+            return queryset.filter(
+                section__semester__year__gt=Semester.latest().year - 5,
+            )
+        return queryset
 
     class Meta:
         model = Instructor
-        fields = ['section__course']
+        fields = ['section__course', 'recent5years']
