a9ebf2bb04f0a45ccdcf7b1023c3f9401cd58fa0
Jihoon Park <bartleby2718@gmail.com>
2020-11-23 19:30:11 +0900

    Fix #259 No semesters when writing a review

--- tcf_website/api/filters.py
+++ tcf_website/api/filters.py
@@ -1,5 +1,5 @@
 """"Custom DRF pagination classes"""
-from django_filters import FilterSet, BooleanFilter, NumberFilter
+from django_filters import FilterSet, NumberFilter
 from ..models import Instructor, Semester
 
 # pylint: disable=unused-argument,no-self-use
@@ -7,17 +7,15 @@ from ..models import Instructor, Semester
 
 class InstructorFilter(FilterSet):
     """Filter for Instructor"""
-    course = NumberFilter(field_name='section__course', distinct=True)
-    recent = BooleanFilter(method='filter_recent', distinct=True)
+    course = NumberFilter(method='filter_recent_instructors_by_course')
 
-    def filter_recent(self, queryset, name, value):
-        """Filter method for most recent 5 years"""
-        if value is True:
-            return queryset.filter(
-                section__semester__year__gte=Semester.latest().year - 5,
-            )
-        return queryset
+    def filter_recent_instructors_by_course(self, queryset, name, value):
+        """Filters instructors who taught the course in the recent 5 years"""
+        return queryset.filter(
+            section__course=value,
+            section__semester__year__gte=Semester.latest().year - 5,
+        ).distinct()
 
     class Meta:
         model = Instructor
-        fields = ['course', 'recent']
+        fields = ['course']
