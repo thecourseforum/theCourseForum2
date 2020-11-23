fd52956e044a8e6bed20d05de25a0039964a625f
Jennifer Long <j.alicia.long@gmail.com>
2020-11-23 01:56:14 -0500

    Update API calls to use recency

--- tcf_website/static/reviews/review_form.js
+++ tcf_website/static/reviews/review_form.js
@@ -7,37 +7,19 @@
 // Executed when DOM is ready
 jQuery(function($) {
     // Clear & disable sequenced dropdowns
+    clearDropdown("#subject");
     clearDropdown("#courseID");
     clearDropdown("#instructor");
+    clearDropdown("#semester");
+    // Enable subject selector, disable the following
+    $("#subject").prop("disabled", false);
     $("#courseID").prop("disabled", true);
     $("#instructor").prop("disabled", true);
-
-    // Fetch all semester data from API
-    var semEndpoint = "http://localhost:8000/api/semesters/";
-    $.getJSON(semEndpoint, function(data) {
-        clearDropdown("#semester"); // Empty dropdown
-
-        // Reverse chronological order (API default is chronological)
-        data.reverse();
-
-        // Generate option tags
-        $.each(data, function(i, semester) {
-            // Most recent 5 years only
-            if (semester.year > 2015) {
-                $("<option />", {
-                    val: semester.id,
-                    text: semester.season + " " + semester.year
-                }).appendTo("#semester");
-            }
-        });
-        return this;
-    });
+    $("#semester").prop("disabled", true);
 
     // Fetch all subdepartment data from API
     var subdeptEndpoint = "http://localhost:8000/api/subdepartments/";
     $.getJSON(subdeptEndpoint, function(data) {
-        clearDropdown("#subject"); // Empty dropdown
-
         // Sort departments alphabetically by mnemonic
         data.sort(function(a, b) {
             return a.mnemonic.localeCompare(b.mnemonic);
@@ -55,18 +37,21 @@ jQuery(function($) {
 
     // Fetch course data on subject select
     $("#subject").change(function() {
-        // Enable course selector
+        // Clear & disable sequenced dropdowns
+        clearDropdown("#courseID");
+        clearDropdown("#instructor");
+        clearDropdown("#semester");
+        // Enable course selector, disable the following
         $("#courseID").prop("disabled", false);
         $("#instructor").prop("disabled", true);
+        $("#semester").prop("disabled", true);
 
         // Fetch course data from API, based on selected subdepartment
         var subdeptID = $("#subject").val();
         var pageSize = "1000";
-        var courseEndpoint = "http://localhost:8000/api/courses/?subdepartment=" + subdeptID + "&page_size=" + pageSize;
+        var courseEndpoint = "http://localhost:8000/api/courses/?subdepartment="
+                             + subdeptID + "&page_size=" + pageSize + "&recent";
         $.getJSON(courseEndpoint, function(data) {
-            clearDropdown("#courseID"); // Empty dropdown
-            clearDropdown("#instructor"); //  Since courses are reloaded
-
             // Generate option tags
             $.each(data.results, function(i, course) {
                 // 4-digit courseIDs only
@@ -83,13 +68,18 @@ jQuery(function($) {
 
     // Fetch instructor data on course select
     $("#courseID").change(function() {
-        // Enable instructor selector
+        // Clear & disable sequenced dropdowns
+        clearDropdown("#instructor");
+        clearDropdown("#semester");
+        // Enable instructor selector, disable the following
         $("#instructor").prop("disabled", false);
+        $("#semester").prop("disabled", true);
 
         // Fetch instructor data from API, based on selected course
         var courseID = $("#courseID").val();
         var pageSize = "1000";
-        var instrEndpoint = "http://localhost:8000/api/instructors/?section__course=" + courseID + "&page_size=" + pageSize;
+        var instrEndpoint = "http://localhost:8000/api/instructors/?section__course="
+                            + courseID + "&page_size=" + pageSize + "&recent=true";
         $.getJSON(instrEndpoint, function(data) {
             clearDropdown("#instructor"); // Empty dropdown
 
@@ -99,6 +89,36 @@ jQuery(function($) {
                     val: instr.id,
                     text: instr.first_name + " " + instr.last_name
                 }).appendTo("#instructor");
+            });
+            return this;
+        });
+    });
+
+    // Fetch semester data on instructor select
+    $("#instructor").change(function() {
+        // Clear & disable sequenced dropdowns
+        clearDropdown("#semester");
+        // Enable semester selector, disable the following
+        $("#semester").prop("disabled", false);
+
+        // Fetch all semester data from API
+        var courseID = $("#courseID").val();
+        var instrID = $("#instructor").val();
+        var semEndpoint = "http://localhost:8000/api/semesters/?course="
+                          + courseID + "&instructor=" + instrID;
+        $.getJSON(semEndpoint, function(data) {
+            // Reverse chronological order (API default is chronological)
+            data.reverse();
+
+            // Generate option tags
+            $.each(data, function(i, semester) {
+                // Most recent 5 years only
+                if (semester.year > 2014) {
+                    $("<option />", {
+                        val: semester.id,
+                        text: semester.season + " " + semester.year
+                    }).appendTo("#semester");
+                }
             });
             return this;
         });
