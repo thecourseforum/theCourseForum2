a885c541a608748c77793f9072572e2473f786a3
Jennifer Long <j.alicia.long@gmail.com>
2020-11-23 03:31:35 -0500

    Put course ID and name together

--- tcf_website/api/urls.py
+++ tcf_website/api/urls.py
@@ -8,7 +8,6 @@ router.register(r'schools', views.SchoolViewSet)
 router.register(r'departments', views.DepartmentViewSet)
 router.register(r'subdepartments', views.SubdepartmentViewSet)
 router.register(r'courses', views.CourseViewSet)
-# router.register(r'courseids', views.CourseIDViewSet)
 router.register(r'instructors', views.InstructorViewSet)
 router.register(r'semesters', views.SemesterViewSet)
 
