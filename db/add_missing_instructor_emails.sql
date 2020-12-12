cb31ecc5c58c5de5da9715e6019dbb1a00e221f6
Jihoon Park <bartleby2718@gmail.com>
2020-12-13 07:10:29 +0900

    Take out the unnecessary assumption

--- db/add_missing_instructor_emails.sql
+++ db/add_missing_instructor_emails.sql
@@ -1,9 +1,7 @@
 -- Copy `email`s from CourseInstructorGrade to Instructor
--- assuming instructor names are unique
 UPDATE tcf_website_instructor AS i
 SET email = cig.email
 FROM tcf_website_courseinstructorgrade AS cig
 WHERE i.email = ''
     AND cig.email <> ''
-    AND cig.last_name = i.last_name
-    AND cig.first_name = i.first_name;
+    AND cig.instructor_id = i.id;
