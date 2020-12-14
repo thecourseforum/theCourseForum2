3b0b4d96ff6b5254c3ec322e39b61228e27e633d
Bartleby <bartleby2718@gmail.com>
2020-12-15 07:13:49 +0900

    Add an if statement to each settings file

--- tcf_core/settings/ci.py
+++ tcf_core/settings/ci.py
@@ -1,4 +1,4 @@
-# pylint: disable=unused-wildcard-import,wildcard-import
+# pylint: disable=unused-wildcard-import,wildcard-import,fixme
 """
 Django settings module for CI services such as Travis or GitHub Actions
 
@@ -7,11 +7,14 @@ okay to use some strings constants here and in .travis.yml or python-app.yml
 """
 from .base import *
 
-DATABASES['default'] = {
-    'NAME': 'tcf_db',
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'USER': 'tcf_django',
-    'PASSWORD': 's3kr1t',
-    'HOST': 'tcf_db',  # TODO: for Travis, localhost for GitHub Actions
-    'PORT': 5432,
-}
+# The following if statement is needed to prevent overwriting global variables when
+# this settings file is interpreted
+if os.environ.get('DJANGO_SETTINGS_MODULE') == 'tcf_core.settings.ci':
+    DATABASES['default'] = {
+        'NAME': 'tcf_db',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'tcf_django',
+        'PASSWORD': 's3kr1t',
+        'HOST': 'tcf_db',  # TODO: for Travis, localhost for GitHub Actions
+        'PORT': 5432,
+    }
--- tcf_core/settings/prod.py
+++ tcf_core/settings/prod.py
@@ -2,12 +2,15 @@
 """Django settings module for production environment like Google App Engine"""
 from .base import *
 
-# SECURITY WARNING: App Engine's security features ensure that it is safe to
-# have ALLOWED_HOSTS = ['*'] when the app is deployed. If you deploy a Django
-# app not on App Engine, make sure to set an appropriate host here.
-# See https://docs.djangoproject.com/en/dev/ref/settings/ (from GCP
-# documentation)
-ALLOWED_HOSTS = ['*']
+# The following if statement is needed to prevent overwriting global variables when
+# this settings file is interpreted
+if os.environ.get('DJANGO_SETTINGS_MODULE') == 'tcf_core.settings.prod':
+    # SECURITY WARNING: App Engine's security features ensure that it is safe to
+    # have ALLOWED_HOSTS = ['*'] when the app is deployed. If you deploy a Django
+    # app not on App Engine, make sure to set an appropriate host here.
+    # See https://docs.djangoproject.com/en/dev/ref/settings/ (from GCP
+    # documentation)
+    ALLOWED_HOSTS = ['*']
 
-# Use secure connection for database access
-DATABASES['default']['OPTIONS'] = {'sslmode': 'require'}
+    # Use secure connection for database access
+    DATABASES['default']['OPTIONS'] = {'sslmode': 'require'}
