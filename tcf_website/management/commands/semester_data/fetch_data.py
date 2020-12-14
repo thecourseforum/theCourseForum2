500aaa01c482f49da2aa57c193cc6d0b0280842b
alex-shen1 <shen.alexander@gmail.com>
2020-12-14 01:18:24 -0500

    Added documentation

--- tcf_website/management/commands/semester_data/fetch_data.py
+++ tcf_website/management/commands/semester_data/fetch_data.py
@@ -1,12 +1,12 @@
 import requests
 
 """
-This script downloads data from Lou's List and converts it into 
+This script downloads data from Lou's List and converts it into
 a usable CSV file. However, in its current state we have to
 manually hardcode the year/season of the semester(s) we want to
-download. 
+download.
 
-Potential todo: create a cron job that runs this script and 
+Potential todo: create a cron job that runs this script and
 `load_semester` every now and then so we don't have to do this.
 """
 
