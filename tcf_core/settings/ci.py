cecb19442f00bb8978624a13199a00326143fc88
Bartleby <bartleby2718@gmail.com>
2020-12-15 06:37:43 +0900

    Fix "server does not support SSL, but SSL was required"

--- tcf_core/settings/ci.py
+++ tcf_core/settings/ci.py
@@ -6,3 +6,12 @@ Unlike other files where we use environment variables for everything, it is
 okay to use some strings constants here and in .travis.yml or python-app.yml
 """
 from .base import *
+
+DATABASES['default'] = {
+    'NAME': 'tcf_db',
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'USER': 'tcf_django',
+    'PASSWORD': 's3kr1t',
+    'HOST': 'tcf_db',  # TODO: for Travis, localhost for GitHub Actions
+    'PORT': 5432,
+}
