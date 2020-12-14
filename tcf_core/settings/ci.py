91b4d8b954ed7cfabe917f0b95856eb5170b1bbf
Bartleby <bartleby2718@gmail.com>
2020-12-15 07:27:04 +0900

    Update ci.py

--- tcf_core/settings/ci.py
+++ tcf_core/settings/ci.py
@@ -10,11 +10,4 @@ from .base import *
 # The following if statement is needed to prevent overwriting global variables when
 # this settings file is interpreted
 if os.environ.get('DJANGO_SETTINGS_MODULE') == 'tcf_core.settings.ci':
-    DATABASES['default'] = {
-        'NAME': 'tcf_db',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER': 'tcf_django',
-        'PASSWORD': 's3kr1t',
-        'HOST': 'tcf_db',  # TODO: for Travis, localhost for GitHub Actions
-        'PORT': 5432,
-    }
+    pass  # Replace this when you need to
