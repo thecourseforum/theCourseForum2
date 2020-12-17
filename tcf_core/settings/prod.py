6b694b981a082fcf033da727468b94e5b71baa7d
Bartleby <bartleby2718@gmail.com>
2020-12-17 12:57:21 +0900

    Add thecourseforum.com to ALLOWED_HOSTS

--- tcf_core/settings/prod.py
+++ tcf_core/settings/prod.py
@@ -10,7 +10,10 @@ if os.environ.get('DJANGO_SETTINGS_MODULE') == 'tcf_core.settings.prod':
     # app not on App Engine, make sure to set an appropriate host here.
     # See https://docs.djangoproject.com/en/dev/ref/settings/ (from GCP
     # documentation)
-    ALLOWED_HOSTS = ['*']
+    # Alex: "When I was trying to do manual GAE deployment when Travis was down,
+    # I had to add thecourseforum.com to ALLOWED_HOSTS or the deployment wouldn't
+    # work (see 40cac033ca14b5c379e5845f9c3870605cdac62d)."
+    ALLOWED_HOSTS = ['*', 'thecourseforum.com']
 
     # Use secure connection for database access
     DATABASES['default']['OPTIONS'] = {'sslmode': 'require'}
