aa98855db393a6d19f27140e89a259f66ce964d9
Jihoon Park <bartleby2718@gmail.com>
2020-12-19 14:09:39 +0900

    Fix pylint errors

--- tcf_website/tests/test_browse.py
+++ tcf_website/tests/test_browse.py
@@ -1,5 +1,5 @@
+# pylint: disable=no-member
 """Tests for browse.py."""
-from django.http import Http404
 from django.test import TestCase
 from django.urls import reverse
 
@@ -13,16 +13,19 @@ class CourseViewTestCase(TestCase):
         setup(self)
 
     def test_legacy_course_url_404(self):
+        """Test if the legacy course view can handle wrong URLs"""
         url = reverse('course_legacy', args=[999999])
         response = self.client.get(url)
-        self.assertEquals(response.status_code, 404)
+        self.assertEqual(response.status_code, 404)
 
     def test_legacy_course_url_redirect(self):
+        """Test if the legacy course view can handle redirects"""
         url = reverse('course_legacy', args=[self.course.id])
         response = self.client.get(url)
         self.assertRedirects(response, '/course/CS/420')
 
     def test_redirect_lowercase_mnemonic(self):
+        """Test if course page URLs with lowercase mnemonics are redirected"""
         url = reverse('course', args=['cs', 421])
         response = self.client.get(url)
         self.assertRedirects(response, '/course/CS/421')
