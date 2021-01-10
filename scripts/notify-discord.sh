c867a70832f4be8ca1d1c58b39a9f95d305bd737
Jihoon Park <bartleby2718@gmail.com>
2021-01-10 17:25:30 +0900

    Fix typo: GITHUB_RUN_NUMBER -> GITHUB_RUN_ID

--- scripts/notify-discord.sh
+++ scripts/notify-discord.sh
@@ -25,7 +25,7 @@ function get_color {
 
 body=$(cat  << EOF
 {
-  "content": "GitHub Actions result for $(get_content). See more at https://github.com/thecourseforum/theCourseForum2/actions/runs/$GITHUB_RUN_NUMBER",
+  "content": "GitHub Actions result for $(get_content). See more at https://github.com/thecourseforum/theCourseForum2/actions/runs/$GITHUB_RUN_ID",
   "embeds": [
     {
       "title": "Pylint",
