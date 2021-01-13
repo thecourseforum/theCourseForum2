4d3564fe418837bed55ff345aeadd1f3bc0b181b
Jihoon Park <bartleby2718@gmail.com>
2021-01-13 15:33:43 +0900

    Add a comment

--- tcf_website/static/course/javascript/course_data.js
+++ tcf_website/static/course/javascript/course_data.js
@@ -79,7 +79,7 @@ const createChart = grades_data => {
                 "#E06A45",  // D
                 "#DE6843",  // D-
                 "#C95F36",  // F
-                "#999900",  // Other
+                "#999900",  // Other (Pass)
                 "#B45133",  // Drop
                 "#b35032",  // Withdraw
             ]
