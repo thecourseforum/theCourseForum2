0107a7ccbe9065553047a02c4ba6146b6456063c
Joe K <joek1301@gmail.com>
2021-01-31 22:18:21 -0500

    Add 'deleted review' message on reviews page (#359)
    * Add 'deleted review' message on reviews page
    
    * Remove extraneous  variable in DeleteReview view.
    
    * Improve comment readability; make successful deletion message more concise
    
    * Add tests for DeleteReview view
    
    * Add suppress_request_warnings to noisy test cases in delete review tests
    
    * Include course title in deletion message
    
    * Remove comment; made obsolete by suppress_request_warnings

--- tcf_website/tests/test_review.py
+++ tcf_website/tests/test_review.py
@@ -1,5 +1,6 @@
 # pylint: disable=no-member
 """Tests for Review model."""
+
 from urllib.parse import urlencode
 
 from django.contrib.messages import get_messages
@@ -77,3 +78,51 @@ class EditReviewTests(TestCase):
         )
         messages = [m.message for m in get_messages(response.wsgi_request)]
         self.assertIn('difficulty', str(messages[0]))
+
+
+class DeleteReviewTests(TestCase):
+    """Tests for the DeleteReview view."""
+
+    def setUp(self):
+        setup(self)  # set up tests: add some example data
+
+    def test_delete_review_message(self):
+        """Test if a message is shown when a user deletes their review."""
+        self.client.force_login(
+            self.review1.user)  # force a login as the author of review1
+        # try and make the user delete review1
+        response = self.client.post(
+            reverse('delete_review', args=[self.review1.id]),
+        )
+
+        self.assertEqual(response.status_code, 302)  # ensure 302 Found status
+
+        # get messages from the request
+        messages = [m.message for m in get_messages(response.wsgi_request)]
+        self.assertEqual(
+            str(messages[0]),
+            f'Successfully deleted your review for {str(self.review1.course)}!')
+
+    @suppress_request_warnings
+    def test_delete_nonexistent_review_id(self):
+        """Test if a 404 is returned for deleting a nonexistent review ID."""
+        self.client.force_login(self.user1)
+        response = self.client.post(
+            reverse('delete_review', args=[0])  # id 0 = nonexistent review
+        )
+
+        self.assertEqual(response.status_code, 404)
+
+    @suppress_request_warnings
+    def test_unauthorized_user_delete(self):
+        """Test if a 403 is returned for unauthorized review deletion."""
+        self.client.force_login(self.user2)  # force login as user2
+
+        response = self.client.post(
+            # try to delete review1, which is not authored by user2
+            reverse('delete_review', args=[self.review1.pk])
+        )
+
+        # trying to delete a review you don't have access to should result in
+        # 403 - forbidden
+        self.assertEqual(response.status_code, 403)
