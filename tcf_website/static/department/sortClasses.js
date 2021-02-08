69ccd29f62c334e34ce4ced21c20e7da458bb6d2
borjainglefernandez <borja.ingle@gmail.com>
2021-02-08 18:19:48 -0500

    Completed Ascending/Descending Sort Buttons

--- tcf_website/static/department/sortClasses.js
+++ tcf_website/static/department/sortClasses.js
@@ -10,6 +10,7 @@ let sortGpaAsc = -1;
     sort_classes takes in a comparing function (defined below) and reorders the classes appropriately
     comparator needed for both ascending and decending as any classes with null values should be at the end of the list either order
 */
+
 const sortByNumber = () => {
     // Updating looks and sorting order
     if (!$("#number-sort-btn").hasClass("active")) {
@@ -17,8 +18,18 @@ const sortByNumber = () => {
         $("#diff-sort-btn").removeClass("active");
         $("#gpa-sort-btn").removeClass("active");
         $("#number-sort-btn").addClass("active");
+        // Ascending order is the default
+        $("#descending-order-btn").removeClass("active");
+        $("#ascending-order-btn").addClass("active");
     } else { // Already selected so simply reverse the order of sorting (ascending vs descending)
         sortNumberAsc *= -1;
+        if ($("#ascending-order-btn").hasClass("active")){
+            $("#ascending-order-btn").removeClass("active");
+            $("#descending-order-btn").addClass("active");
+        } else{
+            $("#descending-order-btn").removeClass("active");
+            $("#ascending-order-btn").addClass("active");
+        }
     }
 
     // Actual sorting
@@ -31,8 +42,17 @@ const sortByRating = () => {
         $("#diff-sort-btn").removeClass("active");
         $("#gpa-sort-btn").removeClass("active");
         $("#rating-sort-btn").addClass("active");
+        $("#descending-order-btn").removeClass("active");
+        $("#ascending-order-btn").addClass("active");
     } else {
         sortRatingAsc *= -1;
+        if ($("#ascending-order-btn").hasClass("active")){
+            $("#ascending-order-btn").removeClass("active");
+            $("#descending-order-btn").addClass("active");
+        } else{
+            $("#descending-order-btn").removeClass("active");
+            $("#ascending-order-btn").addClass("active");
+        }
     }
 
     sortClasses(cmpByProp("rating", sortRatingAsc));
@@ -44,8 +64,17 @@ const sortByDifficulty = () => {
         $("#rating-sort-btn").removeClass("active");
         $("#gpa-sort-btn").removeClass("active");
         $("#diff-sort-btn").addClass("active");
+        $("#descending-order-btn").removeClass("active");
+        $("#ascending-order-btn").addClass("active");
     } else {
         sortDifficultyAsc *= -1;
+        if ($("#ascending-order-btn").hasClass("active")){
+            $("#ascending-order-btn").removeClass("active");
+            $("#descending-order-btn").addClass("active");
+        } else{
+            $("#descending-order-btn").removeClass("active");
+            $("#ascending-order-btn").addClass("active");
+        }
     }
 
     sortClasses(cmpByProp("difficulty", sortDifficultyAsc));
@@ -57,11 +86,54 @@ const sortByGpa = () => {
         $("#rating-sort-btn").removeClass("active");
         $("#diff-sort-btn").removeClass("active");
         $("#gpa-sort-btn").addClass("active");
+        $("#descending-order-btn").removeClass("active");
+        $("#ascending-order-btn").addClass("active");
     } else {
         sortGpaAsc *= -1;
+        if ($("#ascending-order-btn").hasClass("active")){
+            $("#ascending-order-btn").removeClass("active");
+            $("#descending-order-btn").addClass("active");
+        } else{
+            $("#descending-order-btn").removeClass("active");
+            $("#ascending-order-btn").addClass("active");
+        }
     }
 
     sortClasses(cmpByProp("gpa", sortGpaAsc));
+};
+
+const ascendingOrder = () => {
+    if(!$("#ascending-order-btn").hasClass("active")){
+        $("#descending-order-btn").removeClass("active");
+        $("#ascending-order-btn").addClass("active");
+        // Check which sort is currently selected and compute that sort
+        if ($("#number-sort-btn").hasClass("active")) {
+            sortClasses(cmpByProp("title", 1));
+        } else if ($("#rating-sort-btn").hasClass("active")){
+            sortClasses(cmpByProp("rating", -1));
+        } else if ($("#diff-sort-btn").hasClass("active")){
+            sortClasses(cmpByProp("difficulty", 1));
+        } else if ($("#gpa-sort-btn").hasClass("active")){
+            sortClasses(cmpByProp("gpa", -1));
+        }
+    }
+};
+
+const descendingOrder = () => {
+    if(!$("#descending-order-btn").hasClass("active")){
+        $("#ascending-order-btn").removeClass("active");
+        $("#descending-order-btn").addClass("active");
+        // Check which sort is currently selected and compute that sort
+        if ($("#number-sort-btn").hasClass("active")) {
+            sortClasses(cmpByProp("title", -1));
+        } else if ($("#rating-sort-btn").hasClass("active")){
+            sortClasses(cmpByProp("rating", 1));
+        } else if ($("#diff-sort-btn").hasClass("active")){
+            sortClasses(cmpByProp("difficulty", -1));
+        } else if ($("#gpa-sort-btn").hasClass("active")){
+            sortClasses(cmpByProp("gpa", 1));
+        }
+    }
 };
 
 const sortClasses = (cmp) => {
@@ -72,4 +144,4 @@ const sortClasses = (cmp) => {
     });
 };
 
-export { sortByNumber, sortByRating, sortByDifficulty, sortByGpa };
+export { sortByNumber, sortByRating, sortByDifficulty, sortByGpa, ascendingOrder, descendingOrder };
