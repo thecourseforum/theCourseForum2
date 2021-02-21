316cd5561e364d0e8aa525bc32c32e3c215ea544
Jihoon Park <bartleby2718@gmail.com>
2021-02-20 20:50:15 -0500

    Resolve merge conflicts

--- tcf_website/static/course/javascript/course_data.js
+++ tcf_website/static/course/javascript/course_data.js
@@ -1,67 +1,47 @@
 const loadData = data => {
     // order in the input data
+    /* eslint-disable camelcase */
     const { a_plus, a, a_minus, b_plus, b, b_minus, c_plus, c, c_minus, d_plus, d, d_minus, f, ot, drop, withdraw } = data;
     // order we want for the pie chart
     const other = ot;
-    const grades_data = [a_plus, a, a_minus, b_plus, b, b_minus, c_plus, c, c_minus, d_plus, d, d_minus, f, withdraw, drop, other ];
+    const grades_data = [a_plus, a, a_minus, b_plus, b, b_minus, c_plus, c, c_minus, d_plus, d, d_minus, f, withdraw, drop, other];
+    /* eslint-enable camelcase */
 
     createChart(grades_data);
-    
+
     const formatWorkload = x => `${100 * x / data.average_hours_per_week}%`;
     // Subtracting 0.8 from both the numerators and the denominator
     // so that the rating of 1 does look like a low one
     const formatRating = x => `${100 * (x - 0.8) / (5 - 0.8)}%`;
 
     // Pie chart
-    if (exist(data.average_gpa))
-        $(".gpa-text").html(data.average_gpa == 0.0 ? "Pass/Fail" : `${data.average_gpa} GPA`);
-    if(exist(data.total_enrolled))
-        $(".students-text").html(`${data.total_enrolled} Students`);
-    else
-        $(".students-text").remove();
+    if (exist(data.average_gpa)) { $(".gpa-text").html(data.average_gpa === 0.0 ? "Pass/Fail" : `${data.average_gpa} GPA`); }
+    if (exist(data.total_enrolled)) { $(".students-text").html(`${data.total_enrolled} Students`); } else { $(".students-text").remove(); }
 
     // Summary numbers
-    if(exist(data.average_rating))
-        $(".rating-num").html(data.average_rating);
-    if(exist(data.average_hours_per_week))
-        $(".hours-num").html(data.average_hours_per_week);
+    if (exist(data.average_rating)) { $(".rating-num").html(data.average_rating); }
+    if (exist(data.average_hours_per_week)) { $(".hours-num").html(data.average_hours_per_week); }
     // Rating numbers
-    if(exist(data.average_instructor))
-        $(".instructor-num").html(data.average_instructor);
-    if(exist(data.average_fun))
-        $(".fun-num").html(data.average_fun);
-    if(exist(data.average_difficulty))
-        $(".difficulty-num").html(data.average_difficulty);
-    if(exist(data.average_recommendability))
-        $(".recommend-num").html(data.average_recommendability);
+    if (exist(data.average_instructor)) { $(".instructor-num").html(data.average_instructor); }
+    if (exist(data.average_fun)) { $(".fun-num").html(data.average_fun); }
+    if (exist(data.average_difficulty)) { $(".difficulty-num").html(data.average_difficulty); }
+    if (exist(data.average_recommendability)) { $(".recommend-num").html(data.average_recommendability); }
     // Workload numbers
-    if(exist(data.average_amount_reading))
-        $(".reading-num").html(data.average_amount_reading);
-    if(exist(data.average_amount_writing))
-        $(".writing-num").html(data.average_amount_writing);
-    if(exist(data.average_amount_group))
-        $(".group-num").html(data.average_amount_group);
-    if(exist(data.average_amount_homework))
-        $(".homework-num").html(data.average_amount_homework);
+    if (exist(data.average_amount_reading)) { $(".reading-num").html(data.average_amount_reading); }
+    if (exist(data.average_amount_writing)) { $(".writing-num").html(data.average_amount_writing); }
+    if (exist(data.average_amount_group)) { $(".group-num").html(data.average_amount_group); }
+    if (exist(data.average_amount_homework)) { $(".homework-num").html(data.average_amount_homework); }
 
     // Rating bars
-    if(exist(data.average_instructor))
-        $(".instructor-bar").width(formatRating(data.average_instructor));
-    if(exist(data.average_fun))
-        $(".fun-bar").width(formatRating(data.average_fun));
-    if(exist(data.average_difficulty))
-        $(".difficulty-bar").width(formatRating(data.average_difficulty));
-    if(exist(data.average_recommendability))
-        $(".recommend-bar").width(formatRating(data.average_recommendability));
+    if (exist(data.average_instructor)) { $(".instructor-bar").width(formatRating(data.average_instructor)); }
+    if (exist(data.average_fun)) { $(".fun-bar").width(formatRating(data.average_fun)); }
+    if (exist(data.average_difficulty)) { $(".difficulty-bar").width(formatRating(data.average_difficulty)); }
+    if (exist(data.average_recommendability)) { $(".recommend-bar").width(formatRating(data.average_recommendability)); }
     // Workload bars
-    if(exist(data.average_amount_reading))
-        $(".reading-bar").width(formatWorkload(data.average_amount_reading));
-    if(exist(data.average_amount_writing))
-        $(".writing-bar").width(formatWorkload(data.average_amount_writing));
-    if(exist(data.average_amount_group))
-        $(".group-bar").width(formatWorkload(data.average_amount_group));
-    if(exist(data.average_amount_homework))
-        $(".homework-bar").width(formatWorkload(data.average_amount_homework));
+    if (exist(data.average_amount_reading)) { $(".reading-bar").width(formatWorkload(data.average_amount_reading)); }
+    if (exist(data.average_amount_writing)) { $(".writing-bar").width(formatWorkload(data.average_amount_writing)); }
+    if (exist(data.average_amount_group)) { $(".group-bar").width(formatWorkload(data.average_amount_group)); }
+    if (exist(data.average_amount_homework)) { $(".homework-bar").width(formatWorkload(data.average_amount_homework)); }
 };
 
 const createChart = gradesData => {
@@ -69,28 +49,28 @@ const createChart = gradesData => {
         datasets: [{
             data: gradesData,
             backgroundColor: [
-                "#57679D",  // A+
-                "#56669C",  // A
-                "#55659B",  // A-
-                "#384676",  // B+
-                "#384676",  // B
-                "#374575",  // B-
-                "#364474",  // C+
-                "#18244B",  // C
-                "#17234A",  // C-
-                "#162249",  // D+
-                "#E06A45",  // D
-                "#DE6843",  // D-
-                "#C95F36",  // F
-                "#B35032",  // Withdraw
-                "#B45133",  // Drop
-                "#6775A6",  // Other (Pass)
+                "#57679D", // A+
+                "#56669C", // A
+                "#55659B", // A-
+                "#384676", // B+
+                "#384676", // B
+                "#374575", // B-
+                "#364474", // C+
+                "#18244B", // C
+                "#17234A", // C-
+                "#162249", // D+
+                "#E06A45", // D
+                "#DE6843", // D-
+                "#C95F36", // F
+                "#B35032", // Withdraw
+                "#B45133", // Drop
+                "#6775A6" // Other (Pass)
             ]
         }],
         labels: [
             "A+", "A", "A-", "B+", "B", "B-",
             "C+", "C", "C-", "D+", "D", "D-",
-            "F", "Withdraw", "Drop", "Pass",
+            "F", "Withdraw", "Drop", "Pass"
         ]
     };
     var ctx = document.getElementById("myChart");
