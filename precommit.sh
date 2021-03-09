a413177140aaaa17cca039d485cdc58b4779a370
Jihoon Park <bartleby2718@gmail.com>
2021-03-08 21:24:43 -0500

    Change the maximum line length from 79 to 100

--- precommit.sh
+++ precommit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Auto format.
-docker exec tcf_django autopep8 --in-place --jobs=0 --aggressive --aggressive -r tcf_website tcf_core
+docker exec tcf_django autopep8 --in-place --jobs=0 --max-line-length=100 --aggressive --aggressive -r tcf_website tcf_core
 # Lint.
 docker exec tcf_django pylint --jobs=0 --load-plugins pylint_django --django-settings-module=tcf_core.settings tcf_website tcf_core
 docker exec tcf_django node_modules/.bin/eslint --cache --fix tcf_website/static/**/*.js -c .config/.eslintrc.yml
