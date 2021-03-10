21df121cd24417d596e006c661970773e139ca71
alex-shen1 <shen.alexander@gmail.com>
2021-03-09 21:27:13 -0500

    Get new grades data and split it by semester

---
+++ tcf_website/management/commands/grade_data/split_data.py
@@ -0,0 +1,16 @@
+"""
+The data came from UVA was 2009-2020, but that's bulky and we want it to come
+in semester-by-semester files.
+"""
+from pandas import read_csv
+
+raw_data = read_csv("csv/GradeDistributionF09toSpr20.csv", encoding="ISO-8859-1")
+# Default is utf-8, which breaks on certain characters (probably prof names)
+
+grouped_data = raw_data.groupby("Term Desc")  # What semesters are called in the data
+
+for group in grouped_data.groups:
+    semester = group.lower().replace(' ', '_')  # Format semester name e.g. 2020_fall
+
+    semester_data = grouped_data.get_group(group)
+    semester_data.to_csv(f'csv/{semester}.csv', index=False)
