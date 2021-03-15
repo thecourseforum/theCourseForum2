685f46fc6a609ae5284d7a17aa9b917cb3550c65
vn6 <34071587+vn6@users.noreply.github.com>
2021-03-14 22:09:53 -0400

    show card if removed all saved courses

--- tcf_website/static/course/javascript/user_courses.js
+++ tcf_website/static/course/javascript/user_courses.js
@@ -1,17 +1,34 @@
+var num_courses = 0;
+
 function unsaveCourse(course_id, instructor_id) {
     $.ajax({
         type: "GET",
         url: `/unsave_course/${course_id}/${instructor_id}`
     });
 
-    document.getElementById(`course_${saved.course_id}_${saved.instructor_id}`).remove();
+    document.getElementById(`course_${course_id}_${instructor_id}`).remove();
+
+    num_courses--;
+    if(num_courses==0){
+        document.getElementById("saved_courses_block").innerHTML = `
+            <div class="card col p-5 text-center">
+                <div class="card-body">
+                    <h4 class="card-title">
+                        No Saved Courses <i class="far fa-frown-open fa-fw"></i>
+                    </h4>
+                </div>
+            </div>
+            `;
+    }
 }
 
-var buttons = document.getElementsByClassName("btn");
-for(var button in buttons){
-    var x = button.id.split("_");
+var buttons = document.getElementsByClassName("unsave_btn");
+for(var i=0; i<buttons.length; i++){
+    console.log(buttons[i]);
+    var x = buttons[i].id.split("_");
     var course_id = x[1];
     var instructor_id = x[2];
 
+    num_courses++;
     document.addEventListener("click", ()=>unsaveCourse(course_id, instructor_id), false);
 }
