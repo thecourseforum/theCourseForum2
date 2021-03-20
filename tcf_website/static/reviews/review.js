be29e6c3a2e59cad6e14e6981f6ad67867a46f9d
Andrew Niu <andrewmniu@gmail.com>
2021-03-20 19:38:01 -0400

    Hopefully fixed eslint failure

--- tcf_website/static/reviews/review.js
+++ tcf_website/static/reviews/review.js
@@ -1,65 +1,65 @@
 function handleVote(reviewID, isUpvote) {
-  const upvoteCountElem = $(`#review${reviewID} .upvoteCount`);
-  const downvoteCountElem = $(`#review${reviewID} .downvoteCount`);
-  const upvoteCount = parseInt(upvoteCountElem.text());
-  const downvoteCount = parseInt(downvoteCountElem.text());
+    const upvoteCountElem = $(`#review${reviewID} .upvoteCount`);
+    const downvoteCountElem = $(`#review${reviewID} .downvoteCount`);
+    const upvoteCount = parseInt(upvoteCountElem.text());
+    const downvoteCount = parseInt(downvoteCountElem.text());
 
-  let elem;
-  let otherElem;
-  let endpoint;
-  let newUpvoteCount;
-  let newDownvoteCount;
+    let elem;
+    let otherElem;
+    let endpoint;
+    let newUpvoteCount;
+    let newDownvoteCount;
 
-  if (isUpvote) {
-    elem = $(`#review${reviewID} .upvote`);
-    otherElem = $(`#review${reviewID} .downvote`);
-    endpoint = `/reviews/${reviewID}/upvote`;
+    if (isUpvote) {
+        elem = $(`#review${reviewID} .upvote`);
+        otherElem = $(`#review${reviewID} .downvote`);
+        endpoint = `/reviews/${reviewID}/upvote`;
 
-    // If already upvoted, subtract 1.
-    if (elem.hasClass("active")) {
-      newUpvoteCount = upvoteCount - 1;
-      // If already downvoted, add 1 to upvote and subtract 1 from downvote.
-    } else if (otherElem.hasClass("active")) {
-      newUpvoteCount = upvoteCount + 1;
-      newDownvoteCount = downvoteCount - 1;
-      // Otherwise add 1.
+        // If already upvoted, subtract 1.
+        if (elem.hasClass("active")) {
+            newUpvoteCount = upvoteCount - 1;
+        // If already downvoted, add 1 to upvote and subtract 1 from downvote.
+        } else if (otherElem.hasClass("active")) {
+            newUpvoteCount = upvoteCount + 1;
+            newDownvoteCount = downvoteCount - 1;
+        // Otherwise add 1.
+        } else {
+            newUpvoteCount = upvoteCount + 1;
+        }
     } else {
-      newUpvoteCount = upvoteCount + 1;
-    }
-  } else {
-    elem = $(`#review${reviewID} .downvote`);
-    otherElem = $(`#review${reviewID} .upvote`);
-    endpoint = `/reviews/${reviewID}/downvote`;
+        elem = $(`#review${reviewID} .downvote`);
+        otherElem = $(`#review${reviewID} .upvote`);
+        endpoint = `/reviews/${reviewID}/downvote`;
 
-    // If already downvoted, add 1.
+        // If already downvoted, add 1.
+        if (elem.hasClass("active")) {
+            newDownvoteCount = downvoteCount - 1;
+        // If already upvoted, add 1 to downvote and subtract 1 from upvote.
+        } else if (otherElem.hasClass("active")) {
+            newDownvoteCount = downvoteCount + 1;
+            newUpvoteCount = upvoteCount - 1;
+        // Otherwise subtract 1.
+        } else {
+            newDownvoteCount = downvoteCount + 1;
+        }
+    }
+
+    // POST to upvote or downvote endpoint.
+    fetch(endpoint, {
+        method: "post",
+        headers: { "X-CSRFToken": getCookie("csrftoken") }
+    });
+
+    // Update vote text.
+    upvoteCountElem.text(newUpvoteCount);
+    downvoteCountElem.text(newDownvoteCount);
+    //
     if (elem.hasClass("active")) {
-      newDownvoteCount = downvoteCount - 1;
-      // If already upvoted, add 1 to downvote and subtract 1 from upvote.
-    } else if (otherElem.hasClass("active")) {
-      newDownvoteCount = downvoteCount + 1;
-      newUpvoteCount = upvoteCount - 1;
-      // Otherwise subtract 1.
+        elem.removeClass("active");
     } else {
-      newDownvoteCount = downvoteCount + 1;
+        elem.addClass("active");
+        otherElem.removeClass("active");
     }
-  }
-
-  // POST to upvote or downvote endpoint.
-  fetch(endpoint, {
-    method: "post",
-    headers: { "X-CSRFToken": getCookie("csrftoken") },
-  });
-
-  // Update vote text.
-  upvoteCountElem.text(newUpvoteCount);
-  downvoteCountElem.text(newDownvoteCount);
-  //
-  if (elem.hasClass("active")) {
-    elem.removeClass("active");
-  } else {
-    elem.addClass("active");
-    otherElem.removeClass("active");
-  }
 }
 
 export { handleVote };
