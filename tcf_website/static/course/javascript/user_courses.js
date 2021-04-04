7fecd9bdebbec4c218ce448240a21613861092ae
vn6 <34071587+vn6@users.noreply.github.com>
2021-04-04 18:13:38 -0400

    save course order

--- tcf_website/static/course/javascript/user_courses.js
+++ tcf_website/static/course/javascript/user_courses.js
@@ -34,9 +34,43 @@ for(var i=0; i<buttons.length; i++){
     button.addEventListener("click", ()=>unsaveCourse(course, instructor), false);
 }
 
+// source: https://docs.djangoproject.com/en/3.1/ref/csrf/
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
 //Drag and Drop
 $("#savedCoursesList").sortable();
 $("#savedCoursesList").disableSelection();
 $("#savedCoursesList").on( "sortupdate", function( event, ui ) {
     // save course order
+    const moved = ui.item.context;
+    const moved_id = moved.id.split("_")[3];
+    const successor = $("#" + moved.id).next()[0];
+    const successor_id = successor.id.split("_")[3];
+    const csrftoken = getCookie('csrftoken');
+
+    $.ajaxSetup({
+       headers: { "X-CSRFToken": csrftoken }
+    });
+    $.ajax({
+        type: "POST",
+        url: "/saved/reorder",
+        data: {
+           'to_move_id': moved_id,
+           'successor_id':successor_id
+        }
+    });
 } );
