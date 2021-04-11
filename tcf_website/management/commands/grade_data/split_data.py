34a9077438ebe82d3886f7af17dde61a8deb418f
alex-shen1 <shen.alexander@gmail.com>
2021-04-11 02:11:59 -0400

    More requested fixes

--- tcf_website/management/commands/grade_data/split_data.py
+++ tcf_website/management/commands/grade_data/split_data.py
@@ -5,7 +5,7 @@ may come in handy if we ever need to break up a big data file by semester.
 """
 from pandas import read_csv
 
-raw_data = read_csv("csv/GradeDistributionF09toSpr20.csv", encoding="ISO-8859-1")
+raw_data = read_csv("GradeDistributionF09toSpr20.csv", encoding="ISO-8859-1")
 # Default is utf-8, which breaks on certain characters (probably prof names)
 
 grouped_data = raw_data.groupby("Term Desc")  # What semesters are called in the data
