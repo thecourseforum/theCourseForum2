027bb24b4475c4eb7b2e27519eb2f1e82eb996aa
vn6 <34071587+vn6@users.noreply.github.com>
2021-04-23 15:46:10 -0400

    eslint fixes

--- tcf_website/static/course/javascript/save_course.js
+++ tcf_website/static/course/javascript/save_course.js
@@ -1,10 +1,10 @@
 
 function saveCourse() {
-    const notes = $(`#notesField${courseID}`).val();
+    const notes = $(`#notesField${window.courseID}`).val();
 
     $.ajax({
         type: "GET",
-        url: `/save_course/${courseID}/${instructorID}`,
+        url: `/save_course/${window.courseID}/${window.instructorID}`,
         data: { notes: notes }
     });
 
@@ -15,12 +15,12 @@ function saveCourse() {
 function unsaveCourse() {
     $.ajax({
         type: "GET",
-        url: `/unsave_course/${courseID}/${instructorID}`
+        url: `/unsave_course/${window.courseID}/${window.instructorID}`
     });
 
     document.getElementById("saveCourseBtn").style = "display: auto;";
     document.getElementById("unsaveCourseBtn").style = "display: none!important;";
 }
 
-document.getElementById(`saveCourseBtn${courseID}`).addEventListener("click", saveCourse, false);
+document.getElementById(`saveCourseBtn${window.courseID}`).addEventListener("click", saveCourse, false);
 document.getElementById("unsaveCourseBtn").addEventListener("click", unsaveCourse, false);
