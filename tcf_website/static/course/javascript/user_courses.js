ed7b05eee5244aaca557bf196f8a1c55d106ab88
vn6 <34071587+vn6@users.noreply.github.com>
2021-05-19 21:19:46 -0400

    remove redundant getCookie function

--- tcf_website/static/course/javascript/user_courses.js
+++ tcf_website/static/course/javascript/user_courses.js
@@ -50,23 +50,6 @@ for (var i = 0; i < buttons.length; i++) {
     document.getElementById(`saveCourseBtn${id}`).addEventListener("click", () => editCourse(course, instructor, id), false);
 }
 
-// source: https://docs.djangoproject.com/en/3.1/ref/csrf/
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== "") {
-        const cookies = document.cookie.split(";");
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + "=")) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-
 // Drag and Drop
 $("#savedCoursesList").sortable();
 $("#savedCoursesList").disableSelection();
