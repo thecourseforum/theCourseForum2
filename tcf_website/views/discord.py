1fb8bb5cf2bc9571e2c459c554d9023db8602a72
Alex Shen <shen.alexander@gmail.com>
2022-03-22 21:14:57 -0400

    Revert "Maybe fix"
    This reverts commit f854cf8e79ef45927b6c44859015d2cc398130cc.

--- tcf_website/views/discord.py
+++ tcf_website/views/discord.py
@@ -2,11 +2,8 @@
 import os
 import json
 import requests
-import requests_toolbelt.adapters.appengine
 from django.http import JsonResponse
 
-# Needed for requests to work on GAE
-requests_toolbelt.adapters.appengine.monkeypatch()
 
 def post_message(query):
     """Post message to discord server"""
