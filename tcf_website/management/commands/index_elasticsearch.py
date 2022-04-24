595a2adc1c535a11afc4505769bb8c24c10e0324
Jules Le Menestrel <juleslemenestrel@gmail.com>
2022-04-24 17:02:43 -0400

    Add migrations

--- tcf_website/management/commands/index_elasticsearch.py
+++ tcf_website/management/commands/index_elasticsearch.py
@@ -41,7 +41,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         courses_engine_endpoint = os.environ['ELASTICSEARCH_ENDPOINT'] + 'tcf-courses/search'
-        instructors_engine_endpoint = os.environ['ELASTICSEARCH_ENDPOINT'] + 'tcf-instructors/search'
+        instructors_engine_endpoint = os.environ['ELASTICSEARCH_ENDPOINT'] + \
+            'tcf-instructors/search'
         all_courses = Course.objects.all().order_by('pk')
         all_instructors = Instructor.objects.all().order_by('pk')
         self.stdout.write("Number of Courses: " + str(len(all_courses)))
