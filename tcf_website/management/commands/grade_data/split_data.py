c2343f4484e2cf16ad859fc078d078c19819053f
Alex Shen <shen.alexander@gmail.com>
2022-10-31 03:08:36 -0400

    Added + split F21/S22 data

--- tcf_website/management/commands/grade_data/split_data.py
+++ tcf_website/management/commands/grade_data/split_data.py
@@ -4,8 +4,9 @@ in semester-by-semester files. This script probably won't ever be run again, but
 may come in handy if we ever need to break up a big data file by semester.
 """
 from pandas import read_csv
+from sys import argv
 
-raw_data = read_csv("GradeDistributionF09toSpr20.csv", encoding="ISO-8859-1")
+raw_data = read_csv(argv[1], encoding="ISO-8859-1")
 # Default is utf-8, which breaks on certain characters (probably prof names)
 
 grouped_data = raw_data.groupby("Term Desc")  # What semesters are called in the data
