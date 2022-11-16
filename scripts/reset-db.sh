61466b22230625f9409712103b239091e37968c8
Alex Shen <shen.alexander@gmail.com>
2022-11-16 18:47:56 -0500

    Add shebang

--- scripts/reset-db.sh
+++ scripts/reset-db.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 docker exec -i tcf_db psql tcf_db -U tcf_django -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
 docker exec -i tcf_db bash -c 'psql tcf_db -U tcf_django < "app/$DB_FILE"'
