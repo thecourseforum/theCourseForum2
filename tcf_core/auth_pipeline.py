2f310977176795a31d9402fc344240d43e586703
Jack Liu <jackliu612@gmail.com>
2022-12-05 13:47:25 -0500

    finish code logic

--- tcf_core/auth_pipeline.py
+++ tcf_core/auth_pipeline.py
@@ -11,13 +11,11 @@ from django.core.exceptions import ValidationError
 from django.core.mail import send_mail
 from django.urls import reverse
 from django.conf import settings
+from tcf_website.models import User
 
 
 def auth_allowed(backend, details, response, *args, **kwargs):
     """Route unallowed auth attempts to error page."""
-    print('checkingggggggggg')
-    print(response)
-    print(details)
     if not backend.auth_allowed(response, details):
         return redirect('/login/error', error=True)
     return None
@@ -26,22 +24,20 @@ def password_validation(backend, details, request, response, *args, **kwargs):
     """Route unallowed auth attempts to error page."""
     if backend.name != 'email':
         return
-    print(response)
-    print('lmaooooooooooooooooooooo')
-    # print('request:', request, request.POST.get('password'), request.POST.get('password_confirm'))
-    if response.get('password') != response.get('password_confirm'):
-        return render(request, 'login/login_form.html', {
-            'error_message': ['passwords do not match']
-        })
-    try:
-        validate_password(response.get('password'))
-        # details.put(response.password)
-        # details.put(response.password)
-        # details.put(response.password)
-    except ValidationError as e:
-        return render(request, 'login/login_form.html', {
-            'error_message': e
-        })
+    if not response.get('login'):
+        if response.get('password') != response.get('password_confirm'):
+            return render(request, 'login/register_form.html', {
+                'error_message': ['passwords do not match']
+            })
+        try:
+            validate_password(response.get('password'))
+        except ValidationError as e:
+            return render(request, 'login/login_form.html', {
+                'error_message': e
+            })
+    else:
+        if len(User.objects.filter(email=response.get('email'))) == 0:
+            return redirect('/register', error=True)
     return {'password': response.get('password')}
 
 @partial
@@ -61,10 +57,6 @@ def collect_extra_info(
     # because it exists in FIELDS_STORED_IN_SESSION
     grad_year = strategy.session_get('grad_year', None)
     if not grad_year:
-        print('.' * 100)
-        print(strategy)
-        print(backend.name)
-        print('-' * 100)
         # if we return something besides a dict or None, then that is
         # returned to the user -- in this case we will redirect to a
         # view that can be used to get a password
@@ -80,18 +72,16 @@ def create_user(strategy, details, backend, user=None, *args, **kwargs):
 
     Based on https://github.com/python-social-auth/social-core/blob/3.3.3/social_core/pipeline/user.py#L64
     """
-
     # User has registered previously.
     if user:
-        return {'is_new': False}
+        if backend.name == 'email':
+            details['fullname'] = user.full_name()
+            details['first_name'] = user.first_name
+            details['last_name'] = user.last_name
+        return {'is_new': False, 'details': details}
 
     fields = dict((name, kwargs.get(name, details.get(name)))
                   for name in backend.setting('USER_FIELDS', USER_FIELDS))
-    print(details)
-    print('xdddddddddddddddddddddddddd')
-    print(fields)
-    print(backend.setting('USER_FIELDS', USER_FIELDS))
-
     
     if not fields:
         return None
@@ -110,22 +100,14 @@ def create_user(strategy, details, backend, user=None, *args, **kwargs):
 def check_user_password(strategy, backend, user, is_new=False, password="", *args, **kwargs):
     if backend.name != 'email':
         return
-    print('hehehehehehehehehehehehehexd')
-    print(user)
-    print(is_new)
-    print(password)
 
     if is_new:
         user.set_password(password)
         user.save()
     elif not user.check_password(password):
-        # return {'user': None, 'social': None}
-        raise AuthForbidden(backend)
+        return redirect('/login/password_error', error=True)
 
 def validate_email(strategy, backend, code, partial_token):
-    print('GOT HERE!! YAY')
-    print(settings.EMAIL_HOST_USER)
-    print(settings.EMAIL_HOST_PASSWORD)
     if not code.verified:
         url = strategy.build_absolute_uri(
             reverse('social:complete', args=(backend.name,))
