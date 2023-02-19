d5203c397fa1fe7138ff2a3c320199a628569006
Oliver Song <80020297+obs4qqf@users.noreply.github.com>
2023-02-19 12:25:52 -0500

    Started questions form

---
+++ tcf_website/views/qa.py
@@ -0,0 +1,35 @@
+"""View for question and answer creation."""
+
+from django import forms
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.shortcuts import render, redirect
+
+from ..models import Question
+
+
+class QuestionForm(forms.ModelForm):
+    """Form for question creation"""
+    class Meta:
+        model = Question
+        fields = ['text', 'course', 'instructor']
+
+
+@login_required
+def new_review(request):
+    """Question creation view."""
+
+    # Collect form data into Review model instance.
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.user = request.user
+
+            instance.save()
+
+            messages.success(request, f'Successfully added a question for {instance.course}!')
+            return redirect('browse')
+        return render(request, 'browse', {'form': form})
+    return render(request, 'browse')
