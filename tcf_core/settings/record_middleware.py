bdef7747b59a4cf7f2eb95371a33a2ab14e0b2c1
ajnye9 <ajnye9@gmail.com>
2023-04-09 14:24:48 -0400

    Set title limit and fixed list shrink bug

--- tcf_core/settings/record_middleware.py
+++ tcf_core/settings/record_middleware.py
@@ -15,30 +15,29 @@ class RecordMiddleware(object):
             previous_paths_titles = request.COOKIES['previous_paths_titles']
             # Converts string representation of list into list object
             previous_paths_titles = ast.literal_eval(previous_paths_titles)
-
-            # Keeps top 10 items in list
-            # Only keeps 9 here since 1 is inserted immediately after
-            if len(previous_paths) > 9:
-                previous_paths = previous_paths[:9]
-                previous_paths_titles = previous_paths_titles[:9]
         else:
             previous_paths = []
             previous_paths_titles = []
 
         response = self.get_response(request)
         if check_path(request.path) and request.session.get('instructor_fullname') is not None:
-            # previous_paths.append(request.build_absolute_uri())
             previous_paths.insert(0, request.build_absolute_uri())
             previous_paths = list(dict.fromkeys(previous_paths))
-            response.set_cookie('previous_paths', previous_paths)
 
-            title = request.session.get('course_code') + ' - ' + request.session.get('course_title')
+            title = request.session.get('course_code')
             if request.session.get('instructor_fullname') is not None:
                 title += ' - ' + request.session.get('instructor_fullname')
+            title += ' - ' + request.session.get('course_title')
 
-            # previous_paths_titles.append(title)
             previous_paths_titles.insert(0, title)
             previous_paths_titles = list(dict.fromkeys(previous_paths_titles))
+
+            # Keeps top 10 items in list
+            if len(previous_paths) > 10:
+                previous_paths = previous_paths[:10]
+                previous_paths_titles = previous_paths_titles[:10]
+
+            response.set_cookie('previous_paths', previous_paths)
             response.set_cookie('previous_paths_titles', previous_paths_titles)
         return response
 
