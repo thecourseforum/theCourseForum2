7aa386f8e63b4550a87372f9a7550dd5efecea8f
Justin <justinpark04@gmail.com>
2023-10-22 21:54:32 -0400

    reverted changes

--- tcf_website/static/course/javascript/course_data.js
+++ tcf_website/static/course/javascript/course_data.js
@@ -187,7 +187,7 @@ const createChart = gradesData => {
         type: "pie",
         data: chartData,
         options: {
-            cutoutPercentage: 1,
+            cutoutPercentage: 65,
             responsive: true,
             aspectRatio: 1,
             tooltips: {
