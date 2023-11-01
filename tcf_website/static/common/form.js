0d8d02f6fa876a9b00711f31f91a5b2c7ce51814
blairezh <113472440+blairezh@users.noreply.github.com>
2023-10-31 23:05:51 -0400

    added @virginia.edu email requirement to form validation

--- tcf_website/static/common/form.js
+++ tcf_website/static/common/form.js
@@ -2,7 +2,7 @@
 // parameter: eg. var form = document.getElementById("bugform");
 function validateForm(form, email) {
     var valid = form.checkValidity();
-    if (valid === false) {
+    if (valid === false || email.endsWith("@virginia.edu") === false) {
         event.preventDefault();
         event.stopPropagation();
     }
