b0ba165f466db21cda6ebd76dc6b1d175ec3965c
Jay Lalwani <lalwanijay2005@gmail.com>
2023-11-05 16:30:35 -0500

    removed for loop

--- tcf_website/management/commands/semester_data/fetch_sis_data.py
+++ tcf_website/management/commands/semester_data/fetch_sis_data.py
@@ -75,43 +75,47 @@ def find_all_class_numbers(semester):  # this function will be incredibly slow
             print(e)
             break
         for course in data:
-            all_classes.append(course['class_nbr'])
+            all_classes.append(compile_class_data(course['class_nbr']))
         page += 1
+    return all_classes
 
-
-def compile_all_class_data(course_numbers, semester):
+def compile_class_data(course_number, semester):
     """
-        input: course numbers, semester 
-        output: list of course dictionaries to be used for file writing
-        functionality: create a list of all the course data stored in dictionaries to be able to write to a csv file.
+        input: course number, semester 
+        output: course dictionaries to be used for file writing
+        functionality: request and organize course data in dictionaries to be able to write to a csv file.
     """
-    course_list = []
-    for course_number in course_numbers:
-        url = f"https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassDetails?institution=UVA01&term={semester}&class_nbr={course_number}"
-        
-        try:
-            apiResponse = requests.get(url)
-            data = json.loads(apiResponse.text)
-            if data == []:
-                break
-        except Exception as e:
-            print(e)
+    url = f"https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassDetails?institution=UVA01&term={semester}&class_nbr={course_number}"
+    
+    try:
+        apiResponse = requests.get(url)
+        data = json.loads(apiResponse.text)
+        if data == []:
             break
+    except Exception as e:
+        print(e)
+        break
 
-        class_details = data["section_info"]["class_details"]
-        course_dictionary = {
-            "ClassNumber": course_number,
-            "Mnemonic": class_details["subject"],
-            "Number": class_details["catalog_nbr"],
-            # Parser needs to be updated as uses full word instead of abbriveation (Lecture instead of LEC)
-            "Section": class_details["component"],
-            "Type": ,
-            "Units",
-            "Instructor1","Days1","Room1","MeetingDates1",Instructor2,Days2,Room2,MeetingDates2,Instructor3,Days3,Room3,MeetingDates3,Instructor4,Days4,Room4,MeetingDates4,Title,Topic,Status,Enrollment,EnrollmentLimit,Waitlist,Description
+    class_details = data["section_info"]["class_details"]
+    course_dictionary = {
+        "ClassNumber": course_number,
+        "Mnemonic": class_details["subject"],
+        "Number": class_details["catalog_nbr"],
+        "Section": class_details["class_section"],
+        # For Type, Parser needs to be updated to use abbriveation instead of full word (LEC instead of Lecture)
+        "Type": 
+        switch(class_details["component"]) {
+            case "LEC": "Lecture";
+            case "DIS": "Discussion";
+            case "LAB": "Laboratory";
+            default: class_details["component"];
+        },
+        # "units": "1 - 12 units" to "1 - 12", "units": "3 units" to "3"
+        "Units": class_details["units"][0:class_details["units"].find("u") - 1],
+        "Instructor1": ,"Days1","Room1","MeetingDates1",Instructor2,Days2,Room2,MeetingDates2,Instructor3,Days3,Room3,MeetingDates3,Instructor4,Days4,Room4,MeetingDates4,Title,Topic,Status,Enrollment,EnrollmentLimit,Waitlist,Description
 
-        }
-        course_list.append(course_dictionary)
-    pass
+    }
+    return course_dictionary
 
 
 def make_class_request(url):
