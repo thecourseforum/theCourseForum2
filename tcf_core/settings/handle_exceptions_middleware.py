1eb41f245a0ff43bfe11dfc1c50e7e718bc501e5
ajnye9 <ajnye9@gmail.com>
2023-11-09 18:51:42 -0500

    Middleware Error Handling

---
+++ tcf_core/settings/handle_exceptions_middleware.py
@@ -0,0 +1,17 @@
+import sys
+import traceback
+
+
+class HandleExceptionsMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+
+        return response
+
+    def process_exception(self, request, exception):
+        print('Error on Load')
+        print("Internal Server Error: " + request.get_full_path(), file=sys.stderr)
+        print(traceback.format_exc(), file=sys.stderr)
