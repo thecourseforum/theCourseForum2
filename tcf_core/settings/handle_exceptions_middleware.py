2619f64425f383c7ed1431458b82b77c3a5e50d5
ajnye9 <ajnye9@gmail.com>
2023-11-09 19:00:35 -0500

    Pylint fixes

--- tcf_core/settings/handle_exceptions_middleware.py
+++ tcf_core/settings/handle_exceptions_middleware.py
@@ -1,8 +1,10 @@
+"""Middleware for recording errors to print out."""
 import sys
 import traceback
 
 
 class HandleExceptionsMiddleware:
+    """Records information about errors at any point."""
     def __init__(self, get_response):
         self.get_response = get_response
 
@@ -11,6 +13,7 @@ class HandleExceptionsMiddleware:
 
         return response
 
+    # Gets and prints out all errors to terminal for tracking
     def process_exception(self, request, exception):
         print('Error on Load')
         print("Internal Server Error: " + request.get_full_path(), file=sys.stderr)
