98b6c04af4694d3c7d8082f3edceeaa934cffac8
kmd2zjw <98543353+kmd2zjw@users.noreply.github.com>
2024-01-22 12:52:31 -0500

    add starter suggestions for first sprint: time parsing

---
+++ tcf_website/static/schedule/parse_time.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+
+function parseTime(time_string) {
+    // this function will translate the time string to an understandable format
+    // TODO: implement this method
+
+    return 0;
+}
+
+function checkConflict(newTime, times) {
+    // this method will return true if there is conflict with the list of times passed in anmd the newTime
+    // TODO: implement this method
+
+    return true;
+}
+
+function addTimeBlock(newTime, schedule) {
+    // this method will add the time block to a schedule HTML element
+    // TODO: implement this method, NOTE: there can be many ways to do, this is just a starter code
+
+    return true;
+}
