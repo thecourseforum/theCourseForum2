a6fe79fe7f0122517b22d0e11f8d4574b4496001
Jay Lalwani <lalwanijay2005@gmail.com>
2024-01-22 12:52:30 -0500

    SIS_2023_spring.csv created

--- tcf_website/management/commands/semester_data/fetch_sis_data.py
+++ tcf_website/management/commands/semester_data/fetch_sis_data.py
@@ -42,7 +42,8 @@ def retrieve_semester_courses(semester):  # very slow
         '&page=')
     all_classes = []
     page = 1  # Page is initially 1
-    while page==1:  # loads the first 100 courses (page 1) # Loops through every page and extracts classes until it runs out of pages when the while breaks
+    while True:  # loads the first 100 courses (page 1) # Loops through every page and extracts classes until it runs out of pages when the while breaks
+        print(page)
         page_url = semester_url + str(page)
         try:
             apiResponse = requests.get(page_url)
@@ -59,8 +60,11 @@ def retrieve_semester_courses(semester):  # very slow
             if not class_data:
                 continue
             all_classes.append(class_data)  # adds dict to list of all classes
+        print("Writing to CSV")
+        write_csv(all_classes, f"SIS_2023_spring") # write in chunks to save memory
+        all_classes = []  # resets list of classes to empty
         page += 1  # Incrementing page count so next query will be on next page
-    return all_classes
+    return None
 
 
 def compile_course_data(course_number, semester):
@@ -104,15 +108,15 @@ def compile_course_data(course_number, semester):
         "Days1": meetings[0]["meets"] if meetings[0]["meets"] != "-" else "TBA",
         "Room1": meetings[0]["room"] if meetings[0]["room"] != "-" else "TBA",
         "MeetingDates1": meetings[0]["date_range"] if meetings[0] else "",
-        "Instructor2": ", ".join(instructor["name"] for instructor in meetings[1]["instructors"]) if meetings[1] else "",
+        "Instructor2": ", ".join(instructor["name"] for instructor in meetings[1]["instructors"] if instructor["name"] != "-") if meetings[1] else "",
         "Days2": meetings[1]["meets"] if meetings[1] else "",
         "Room2": meetings[1]["room"] if meetings[1] else "",
         "MeetingDates2": meetings[1]["date_range"] if meetings[1] else "",
-        "Instructor3": ", ".join(instructor["name"] for instructor in meetings[2]["instructors"]) if meetings[2] else "",
+        "Instructor3": ", ".join(instructor["name"] for instructor in meetings[2]["instructors"] if instructor["name"] != "-") if meetings[2] else "",
         "Days3": meetings[2]["meets"] if meetings[2] else "",
         "Room3": meetings[2]["room"] if meetings[2] else "",
         "MeetingDates3": meetings[2]["date_range"] if meetings[2] else "",
-        "Instructor4": ", ".join(instructor["name"] for instructor in meetings[3]["instructors"]) if meetings[3] else "",
+        "Instructor4": ", ".join(instructor["name"] for instructor in meetings[3]["instructors"] if instructor["name"] != "-") if meetings[3] else "",
         "Days4": meetings[3]["meets"] if meetings[3] else "",
         "Room4": meetings[3]["room"] if meetings[3] else "",
         "MeetingDates4": meetings[3]["date_range"] if meetings[3] else "",
@@ -122,7 +126,7 @@ def compile_course_data(course_number, semester):
         "Enrollment": class_availability["enrollment_total"],
         "EnrollmentLimit": class_availability["class_capacity"],
         "Waitlist": class_availability["wait_list_total"],
-        "Description": data["section_info"]["catalog_descr"]["crse_catalog_description"]
+        "Description": data["section_info"]["catalog_descr"]["crse_catalog_description"].replace("\n", " ").replace("\r", " ")
     }
     return course_dictionary
 
@@ -130,14 +134,15 @@ def compile_course_data(course_number, semester):
 def write_csv(courseList, filename):
     fieldnames = list(courseList[0].keys())
     csv_filename = filename + ".csv"
-    with open(csv_filename, 'w', newline='') as csvfile:
+    with open(csv_filename, 'a', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
+        if csvfile.tell() == 0:
+            writer.writeheader()
         for course in courseList:
             writer.writerow(course)
 
 
-write_csv(retrieve_semester_courses("1232"), "SIS_2023_spring")
+retrieve_semester_courses("1232")
 
 # test SIS data against Lous List data (remove function later)
 
@@ -148,11 +153,13 @@ def compare_csv_files():
         with open('tcf_website/management/commands/semester_data/csv/2023_spring.csv', 'r') as lous_file:
             local_reader = csv.reader(lous_file)
             for sis_row, local_row in zip(sis_reader, local_reader):
-                if sis_row != local_row:
-                    print("Discrepancy:")
-                    print("SIS row: ", sis_row)
-                    print("Local row: ", local_row)
-                    input("Enter to continue...\n")
+                for sis_col, local_col in zip(sis_row, local_row):
+                    if sis_col != local_col:
+                        print("Course #:", sis_row[0])
+                        print("Discrepancy:")
+                        print("SIS: ", sis_col)
+                        print("Lou's: ", local_col)
+                        input("Enter to continue...\n")
 
 
-compare_csv_files()
+# compare_csv_files()
