cee3b1f7b9e6b17b1bf57ee905f5a79eba62affb
Barrett Ruth <br.barrettruth@gmail.com>
2024-01-28 15:40:58 -0500

    feat(format): format filles w/ pre-commit

--- tcf_core/settings/handle_exceptions_middleware.py
+++ tcf_core/settings/handle_exceptions_middleware.py
@@ -5,6 +5,7 @@ import traceback
 
 class HandleExceptionsMiddleware:
     """Records information about errors at any point."""
+
     def __init__(self, get_response):
         self.get_response = get_response
 
@@ -15,7 +16,9 @@ class HandleExceptionsMiddleware:
 
     def process_exception(self, request, exception):
         """Gets and prints out all errors to terminal for tracking"""
-        print('Error on Load')
-        print("Internal Server Error: " + request.get_full_path(), file=sys.stderr)
+        print("Error on Load")
+        print(
+            "Internal Server Error: " + request.get_full_path(), file=sys.stderr
+        )
         print(traceback.format_exc(), file=sys.stderr)
         print(exception)
--- tcf_core/urls.py
+++ tcf_core/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('tcf_website.urls')),
-    path('oauth/', include('social_django.urls')),
-    path('admin/', admin.site.urls),
+    path("", include("tcf_website.urls")),
+    path("oauth/", include("social_django.urls")),
+    path("admin/", admin.site.urls),
 ]
