b026f7779d33b48b8d11bb17ef4c9c20426d13a9
Barrett Ruth <62671086+barrett-ruth@users.noreply.github.com>
2024-01-28 16:30:25 -0600

    Update reset-db.sh

--- scripts/reset-db.sh
+++ scripts/reset-db.sh
@@ -10,4 +10,4 @@ if [ ! -f "$DB_FILE" ]; then
 fi
 
 docker exec -i tcf_db psql tcf_db -U tcf_django -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
-docker exec -i tcf_db bash -c "psql tcf_db -U tcf_django < $DB_FILE"
+docker exec -i tcf_db psql tcf_db -U tcf_django < "$DB_FILE"
