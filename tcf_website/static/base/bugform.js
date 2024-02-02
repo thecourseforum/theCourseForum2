52e81626dd5e77371b6ffb31551cc875a2a7a801
Barrett Ruth <br.barrettruth@gmail.com>
2024-02-02 13:50:01 -0500

    feat(format): new prettier settings

--- tcf_website/static/base/bugform.js
+++ tcf_website/static/base/bugform.js
@@ -1,44 +1,44 @@
 import { validateForm } from "../common/form.js";
 
 function submitForm() {
-    const form = document.getElementById("bugform");
-    const valid = validateForm(form);
-    if (valid === true) {
-        postToDiscord();
-        // close form after submit
-        $("#bugModal").modal("toggle");
-        $("#confirmationModal").modal("toggle");
-    }
+  const form = document.getElementById("bugform");
+  const valid = validateForm(form);
+  if (valid === true) {
+    postToDiscord();
+    // close form after submit
+    $("#bugModal").modal("toggle");
+    $("#confirmationModal").modal("toggle");
+  }
 }
 
 function resetForm() {
-    const form = document.getElementById("bugform");
-    form.classList.remove("was-validated");
-    const emailField = document.getElementById("emailField");
-    emailField.value = "";
-    const descriptionField = document.getElementById("descriptionField");
-    descriptionField.value = "";
+  const form = document.getElementById("bugform");
+  form.classList.remove("was-validated");
+  const emailField = document.getElementById("emailField");
+  emailField.value = "";
+  const descriptionField = document.getElementById("descriptionField");
+  descriptionField.value = "";
 
-    for (let i = 1; i <= 4; i++) {
-        const id = "#category" + i;
-        $(id).prop("checked", false);
-    }
+  for (let i = 1; i <= 4; i++) {
+    const id = "#category" + i;
+    $(id).prop("checked", false);
+  }
 }
 
 function postToDiscord() {
-    const url = window.location.href;
-    const email = $("#emailField").val();
-    const description = $("#descriptionField").val();
-    let categories = "";
-    for (let i = 1; i <= 4; i++) {
-        const id = "#category" + i;
-        if ($(id).is(":checked")) {
-            categories += "[" + $(id).val() + "]";
-        }
+  const url = window.location.href;
+  const email = $("#emailField").val();
+  const description = $("#descriptionField").val();
+  let categories = "";
+  for (let i = 1; i <= 4; i++) {
+    const id = "#category" + i;
+    if ($(id).is(":checked")) {
+      categories += "[" + $(id).val() + "]";
     }
-    const data = {
-        type: "bug",
-        content:
+  }
+  const data = {
+    type: "bug",
+    content:
       "Bug Found! \n**URL:** " +
       url +
       "\n**Description**: \n" +
@@ -46,19 +46,19 @@ function postToDiscord() {
       "\n**Categories: **" +
       categories +
       "\n**Email:** " +
-      email
-    };
+      email,
+  };
 
-    $.ajax({
-        type: "GET",
-        url: "/discord/",
-        data
-    });
+  $.ajax({
+    type: "GET",
+    url: "/discord/",
+    data,
+  });
 }
 
 document
-    .getElementById("bugSubmitBtn")
-    .addEventListener("click", submitForm, false);
+  .getElementById("bugSubmitBtn")
+  .addEventListener("click", submitForm, false);
 document
-    .getElementById("bugFormOpen")
-    .addEventListener("click", resetForm, false);
+  .getElementById("bugFormOpen")
+  .addEventListener("click", resetForm, false);
