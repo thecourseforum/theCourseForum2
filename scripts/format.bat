6ff2988222fbb0e7624ae53a502bf8e73d738cd4
Barrett Ruth <br.barrettruth@gmail.com>
2024-02-02 13:39:55 -0500

    feat(format): format script

---
+++ scripts/format.bat
@@ -0,0 +1,6 @@
+@echo off
+
+docker exec tcf_django black --line-length 80 .
+docker exec tcf_django isort --profile black --line-length 80 .
+docker exec tcf_django npx prettier --write "**/*.{css,js,md}"
+docker exec tcf_django npx eslint -c .config/.eslintrc.yml tcf_website/static/
