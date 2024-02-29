bb7077e02873fbe213141b56718694e89ca27dd6
Barrett Ruth <br.barrettruth@gmail.com>
2024-02-29 12:10:39 -0600

    fix: more formatting

--- tcf_website/management/commands/semester_data/fetch_sis_data.py
+++ tcf_website/management/commands/semester_data/fetch_sis_data.py
@@ -12,12 +12,13 @@ Potential todo: create a cron job that runs this script and
 # however, this initial query is less detailed, so we would take this data to form queries for each class seperately
 # using the course_nbr variable. Then this data is written to a csv.
 
+import csv
+import json
+
 # format -ClassNumber,Mnemonic,Number,Section,Type,Units,Instructor1,Days1,Room1,MeetingDates1,Instructor2,Days2,Room2,MeetingDates2,Instructor3,Days3,Room3,MeetingDates3,Instructor4,Days4,Room4,MeetingDates4,Title,Topic,Status,Enrollment,EnrollmentLimit,Waitlist,Description
 # example call url
 # -https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassDetails?institution=UVA01&term=1242&class_nbr=16634&
 import requests
-import json
-import csv
 
 # url to find all courses in department for a semester to update semester Replace 1228 with the appropriate term.
 # The formula is “1” + [2 digit year] + [2 for Spring, 8 for Fall]. So, 1228 is Fall 2022.
@@ -34,12 +35,14 @@ def retrieve_semester_courses(semester):  # very slow
     output: list of dictionaries where each dictionary is all a course's information for the csv writing
     functionality: connects with sis API and looks at each class. It finds each course's course-number then passes it to compile_course_data
      which returns a dictionary of all of a course's information, which is added to a list containing the course info for all classes.
-     This is done using a page by page approach where all the courses are analyzed one page at a time."""
+     This is done using a page by page approach where all the courses are analyzed one page at a time.
+    """
     semester_url = (
-        'https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.' +
-        'FieldFormula.IScript_ClassSearch?institution=UVA01&term=' +
-        semester +
-        '&page=')
+        "https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH."
+        + "FieldFormula.IScript_ClassSearch?institution=UVA01&term="
+        + semester
+        + "&page="
+    )
     all_classes = []
     page = 1  # Page is initially 1
     while True:  # loads the first 100 courses (page 1)
@@ -50,19 +53,27 @@ def retrieve_semester_courses(semester):  # very slow
             apiResponse = requests.get(page_url)
             page_data = json.loads(apiResponse.text)
         except requests.exceptions.RequestException as e:
-            print(f'An error occurred: {e}')
+            print(f"An error occurred: {e}")
             break
-        if not page_data:  # checks to see if there is no data for that page which means that all
+        if (
+            not page_data
+        ):  # checks to see if there is no data for that page which means that all
             # data has been extracted from previous pages so the loop breaks
             break
 
-        for course in page_data:  # loops through every course on the page and calls compile_course_data and adds output to list
+        for (
+            course
+        ) in (
+            page_data
+        ):  # loops through every course on the page and calls compile_course_data and adds output to list
             # calls function to create dict of class info
-            class_data = compile_course_data(course['class_nbr'], semester)
+            class_data = compile_course_data(course["class_nbr"], semester)
             if not class_data:
                 continue
             all_classes.append(class_data)  # adds dict to list of all classes
-        write_csv(all_classes, f"SIS_2023_spring")  # write in chunks to save memory
+        write_csv(
+            all_classes, f"SIS_2023_spring"
+        )  # write in chunks to save memory
         all_classes = []  # resets list of classes to empty
         page += 1  # Incrementing page count so next query will be on next page
     return None
@@ -70,16 +81,16 @@ def retrieve_semester_courses(semester):  # very slow
 
 def compile_course_data(course_number, semester):
     """
-        input: course number, semester
-        output: course dictionaries to be used for file writing
-        functionality: request and organize course data in dictionaries to be able to write to a csv file.
+    input: course number, semester
+    output: course dictionaries to be used for file writing
+    functionality: request and organize course data in dictionaries to be able to write to a csv file.
     """
     url = f"https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassDetails?institution=UVA01&term={semester}&class_nbr={course_number}"
 
     try:
         apiResponse = requests.get(url)
     except requests.exceptions.RequestException as e:
-        print(f'An error occurred: {e}')
+        print(f"An error occurred: {e}")
         return None
 
     data = json.loads(apiResponse.text)
@@ -103,22 +114,56 @@ def compile_course_data(course_number, semester):
         "Type": {
             "LEC": "Lecture",
             "DIS": "Discussion",
-            "LAB": "Laboratory"
+            "LAB": "Laboratory",
         }.get(class_details["component"], class_details["component"]),
-        "Units": class_details["units"][0:class_details["units"].find("units") - 1],
-        "Instructor1": ", ".join(instructor["name"] for instructor in meetings[0]["instructors"] if instructor["name"] != "-") if meetings[0] else "",
+        "Units": class_details["units"][
+            0 : class_details["units"].find("units") - 1
+        ],
+        "Instructor1": (
+            ", ".join(
+                instructor["name"]
+                for instructor in meetings[0]["instructors"]
+                if instructor["name"] != "-"
+            )
+            if meetings[0]
+            else ""
+        ),
         "Days1": meetings[0]["meets"] if meetings[0]["meets"] != "-" else "TBA",
         "Room1": meetings[0]["room"] if meetings[0]["room"] != "-" else "TBA",
         "MeetingDates1": meetings[0]["date_range"] if meetings[0] else "",
-        "Instructor2": ", ".join(instructor["name"] for instructor in meetings[1]["instructors"] if instructor["name"] != "-") if meetings[1] else "",
+        "Instructor2": (
+            ", ".join(
+                instructor["name"]
+                for instructor in meetings[1]["instructors"]
+                if instructor["name"] != "-"
+            )
+            if meetings[1]
+            else ""
+        ),
         "Days2": meetings[1]["meets"] if meetings[1] else "",
         "Room2": meetings[1]["room"] if meetings[1] else "",
         "MeetingDates2": meetings[1]["date_range"] if meetings[1] else "",
-        "Instructor3": ", ".join(instructor["name"] for instructor in meetings[2]["instructors"] if instructor["name"] != "-") if meetings[2] else "",
+        "Instructor3": (
+            ", ".join(
+                instructor["name"]
+                for instructor in meetings[2]["instructors"]
+                if instructor["name"] != "-"
+            )
+            if meetings[2]
+            else ""
+        ),
         "Days3": meetings[2]["meets"] if meetings[2] else "",
         "Room3": meetings[2]["room"] if meetings[2] else "",
         "MeetingDates3": meetings[2]["date_range"] if meetings[2] else "",
-        "Instructor4": ", ".join(instructor["name"] for instructor in meetings[3]["instructors"] if instructor["name"] != "-") if meetings[3] else "",
+        "Instructor4": (
+            ", ".join(
+                instructor["name"]
+                for instructor in meetings[3]["instructors"]
+                if instructor["name"] != "-"
+            )
+            if meetings[3]
+            else ""
+        ),
         "Days4": meetings[3]["meets"] if meetings[3] else "",
         "Room4": meetings[3]["room"] if meetings[3] else "",
         "MeetingDates4": meetings[3]["date_range"] if meetings[3] else "",
@@ -128,7 +173,11 @@ def compile_course_data(course_number, semester):
         "Enrollment": class_availability["enrollment_total"],
         "EnrollmentLimit": class_availability["class_capacity"],
         "Waitlist": class_availability["wait_list_total"],
-        "Description": data["section_info"]["catalog_descr"]["crse_catalog_description"].replace("\n", " ").replace("\r", " ")
+        "Description": data["section_info"]["catalog_descr"][
+            "crse_catalog_description"
+        ]
+        .replace("\n", " ")
+        .replace("\r", " "),
     }
     return course_dictionary
 
@@ -136,7 +185,7 @@ def compile_course_data(course_number, semester):
 def write_csv(course_list, filename):
     fieldnames = list(course_list[0].keys())
     csv_filename = filename + ".csv"
-    with open(csv_filename, 'a', newline='') as csvfile:
+    with open(csv_filename, "a", newline="") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         if csvfile.tell() == 0:
             writer.writeheader()
@@ -150,9 +199,12 @@ retrieve_semester_courses("1232")
 
 
 def compare_csv_files():
-    with open('SIS_2023_spring.csv', 'r') as sis_file:
+    with open("SIS_2023_spring.csv", "r") as sis_file:
         sis_reader = csv.reader(sis_file)
-        with open('tcf_website/management/commands/semester_data/csv/2023_spring.csv', 'r') as lous_file:
+        with open(
+            "tcf_website/management/commands/semester_data/csv/2023_spring.csv",
+            "r",
+        ) as lous_file:
             local_reader = csv.reader(lous_file)
             for sis_row, local_row in zip(sis_reader, local_reader):
                 for sis_col, local_col in zip(sis_row, local_row):
