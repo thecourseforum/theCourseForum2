0693dfa04bf389fc705598a35472ec39e4043926
kmd2zjw <98543353+kmd2zjw@users.noreply.github.com>
2024-03-06 15:15:36 -0500

    add ability to edit schedule names and courses

--- tcf_website/urls.py
+++ tcf_website/urls.py
@@ -55,6 +55,7 @@ urlpatterns = [
     path('schedule/new/', views.new_schedule, name='new_schedule'),
     path('schedule/delete/', views.delete_schedule, name='delete_schedule'),
     path('schedule/edit/', views.edit_schedule, name='edit_schedule'),
+    path('schedule/modal/editor', views.modal_load_editor, name='modal_load_editor'),
     path('schedule/modal/sections/', views.modal_load_sections, name='modal_load_sections'),
     path('schedule/modal/<str:mode>/', views.view_schedules_modal, name='modal_load_schedules'),
     path('schedule/add_course/', views.schedule_add_course, name='schedule_add_course'),
