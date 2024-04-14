7d81e8eedb71750f3ad50d6e9e507dd4e4a57708
Bhyun <superay190@gmail.com>
2024-04-14 14:34:52 -0400

    Changed Dependency on CSV

--- tcf_website/views/recommender.py
+++ tcf_website/views/recommender.py
@@ -10,7 +10,7 @@ import os
 
 # load_dotenv()
 
-N = 5  # number of similar courses to print
+N = 2  # number of similar courses to print
 CHUNKS = 30  # number of chunks in digital ocean
 access_key = os.getenv("DO_ACCESS_KEY")
 secret_key = os.getenv("DO_SECRET_KEY")
@@ -43,30 +43,67 @@ def load_similarity_matrix_from_spaces(space_name, object_key):
         return similarity_matrix
 
     except NoCredentialsError:
-        print("Credentials not available or not valid.")
+        return []
+        # print("Credentials not available or not valid.")
 
+def load_mapping_from_spaces(space_name, object_key, object_key2):
+    try:
+        # Create an S3 client
+        s3 = boto3.client('s3',
+                          region_name="nyc3",
+                          endpoint_url='https://nyc3.digitaloceanspaces.com',
+                          aws_access_key_id=access_key,
+                          aws_secret_access_key=secret_key,
+                          config=boto3.session.Config(signature_version='s3v4', retries={
+                              'max_attempts': 2,
+                              'mode': 'standard'
+                          },
+                                                      s3={'addressing_style': "virtual"}, ))
 
-def recommend_classes(similarity_matrix, spring_data, close_match, index_of_the_class, chunk_val, chunk_size):
+        response = s3.get_object(Bucket=space_name, Key=object_key)
+        response2 = s3.get_object(Bucket=space_name, Key=object_key2)
+        body_content = response['Body'].read()
+        body_content2 = response2['Body'].read()
+        buffer = BytesIO(body_content)
+        buffer2 = BytesIO(body_content2)
+
+        row_to_class_mapping = np.load(buffer, allow_pickle=True).item()  # .item() to load as dictionary
+        class_to_index_map = np.load(buffer2, allow_pickle=True).item()  # .item() to load as dictionary
+        return [row_to_class_mapping, class_to_index_map]
+
+    except NoCredentialsError:
+        return []
+        # print("Credentials not available or not valid.")
+
+def recommend_classes(similarity_matrix, adjusted_index_of_the_class, index_to_course_map, og_index_of_class):
     # getting a list of similar movies
-    similarity_score = list(enumerate(similarity_matrix[index_of_the_class]))
+    similarity_score = list(enumerate(similarity_matrix[adjusted_index_of_the_class]))
 
     # sorting the movies based on their similarity score
     sorted_similar_courses = sorted(similarity_score, key=lambda x: x[1], reverse=True)
+    # print(sorted_similar_courses)
 
     i = 1
     displayedSuggestions = []
     course_mnemonics = []
     course_numbers = []
-    inputCourseMnemonic = \
-    spring_data[spring_data.index == (index_of_the_class + chunk_size * chunk_val)]['Mnemonic'].values[0]
-    inputCourseNumber = spring_data[spring_data.index == (index_of_the_class + chunk_size * chunk_val)]['Number'].values[0]
+    inputCourseMnemonic = index_to_course_map[og_index_of_class][0]
+    inputCourseNumber = index_to_course_map[og_index_of_class][1] 
+    # input_course = get_object_or_404(
+    #     Course, subdepartment__mnemonic=inputCourseMnemonic.upper().strip(), number=int(inputCourseNumber)
+    # )
     for course in sorted_similar_courses:
         index = course[0]
-        title_from_index = spring_data[spring_data.index == index]['Title'].values[0]
-        mnemonic_from_index = spring_data[spring_data.index == index]['Mnemonic'].values[0]
-        number_from_index = spring_data[spring_data.index == index]['Number'].values[0]
+        mnemonic_from_index = index_to_course_map[index][0]
+        number_from_index = index_to_course_map[index][1]
+        print(mnemonic_from_index, number_from_index)
+        # similar_course = get_object_or_404(
+        #     Course, subdepartment__mnemonic=mnemonic_from_index.upper().strip(), number=int(number_from_index)
+        # )
+        # failtest = similar_course.title
+        title_from_index = "banana" + str(index)
         if (i < N + 1):
-            if (title_from_index not in displayedSuggestions and title_from_index != close_match
+            if (title_from_index not in displayedSuggestions and title_from_index != 'tree'
                     and mnemonic_from_index == inputCourseMnemonic and number_from_index >= inputCourseNumber):
                 displayedSuggestions.append(title_from_index)
                 course_mnemonics.append(mnemonic_from_index)
@@ -79,30 +116,34 @@ def recommend_classes(similarity_matrix, spring_data, close_match, index_of_the_
     return [displayedSuggestions, course_mnemonics, course_numbers]
 
 
-def get_recommendations_helper(course_name):
+def get_recommendations_helper(course_name, course_mnemonic, course_number):
     # Replace with your DigitalOcean Space name, access key, and secret key
     space_name = 'recommender'
 
     # Load data
-    spring_data = pd.read_csv('tcf_website/management/commands/semester_data/csv/2024_spring.csv')
+    # spring_data = pd.read_csv('tcf_website/management/commands/semester_data/csv/2024_spring.csv')
     # Load data for testing python file individually
     # spring_data = pd.read_csv('../management/commands/semester_data/csv/2024_spring.csv')
 
-    list_of_all_titles = spring_data['Title'].tolist()
+    # list_of_all_titles = spring_data['Title'].tolist()
+    mapping_key1 = 'row_to_class_mapping.npy'  # Provide the correct object key for the mapping file
+    mapping_key2 = 'class_to_index.npy'
 
-    # finding the close match for the movie name given by the user
-    find_close_match = difflib.get_close_matches(course_name, list_of_all_titles)
+    row_to_class_mapping, class_to_index_map = load_mapping_from_spaces(space_name, mapping_key1, mapping_key2)
+
+    # finding the close match for the class name given by the user
+    find_close_match = difflib.get_close_matches(course_mnemonic + str(course_number), class_to_index_map.keys())
 
     if(len(find_close_match) < 1):
         return []
     
     close_match = find_close_match[0]
 
-    # finding the index of the movie with title
-    index_of_the_class = spring_data[spring_data.Title == close_match].index[0]
+    # finding the index of the class with title
+    index_of_the_class = class_to_index_map[close_match]
 
     # load chunk from digital ocean that contains similarities for specified class
-    chunk_size = int(len(list_of_all_titles) / CHUNKS)
+    chunk_size = int(len(row_to_class_mapping.keys()) / CHUNKS)
     chunk_val = int(index_of_the_class / chunk_size)
     object_key = f'chunk_{chunk_val}.npy'
     adjusted_index_of_class = int(index_of_the_class % chunk_size)
@@ -111,24 +152,11 @@ def get_recommendations_helper(course_name):
     similarity_matrix = load_similarity_matrix_from_spaces(space_name, object_key)
 
     # Get recommendations
-    recommendations = recommend_classes(similarity_matrix, spring_data, close_match, adjusted_index_of_class, chunk_val,
-                                        chunk_size)
+    recommendations = recommend_classes(similarity_matrix, adjusted_index_of_class, row_to_class_mapping, index_of_the_class)
     
     return recommendations
-
-
-def get_recommendations(request, mnemonic, course_number):
-    course = get_object_or_404(
-        Course, subdepartment__mnemonic=mnemonic.upper(), number=course_number)
-    request.session['course_code'] = course.code()
-    recommendations = get_recommendations_helper(course)
-    return render(request, 'course/course.html',
-                  {
-                      'recommendations': recommendations
-                  })
-
 
 # For Testing:
 # Instructions: uncomment main below, comment out "from modules import" segment, and comment out "get_recommendations" function, comment correct load_csv function
 # if __name__ == "__main__":
-#     print(get_recommendations_helper("Thermodynamics and Phase Equilibria of Materials"))
+#     print(get_recommendations_helper("Data Structures and Algorithms 1", "CS", 2100))
