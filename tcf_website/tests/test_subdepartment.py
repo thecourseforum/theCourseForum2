8b6a0b2c329e8dcfde62e5eef16050ab538ca037
artiehumphreys <artieh2003@gmail.com>
2024-06-15 17:54:58 -0400

    feat: deprecated subdepartment tests

--- tcf_website/tests/test_subdepartment.py
+++ tcf_website/tests/test_subdepartment.py
@@ -17,24 +17,6 @@ class SubdepartmentTestCase(TestCase):
         """Test __str__ method in Subdepartment model"""
         self.assertEqual("CS - Computer Science", str(self.subdepartment))
 
-    def test_recent_courses_has_recent_courses(self):
-        """Test recent_courses method in Subdepartment model when there are recent courses"""
-        recent_courses = Course.objects.filter(
-            title__in=[self.course.title, self.course2.title]
-        )
-
-        self.assertQuerysetEqual(
-            self.subdepartment.recent_courses(),
-            recent_courses,
-            transform=lambda x: x,  # Needed so that the formatting works
-            ordered=False,
-        )
-
-    def test_recent_courses_has_no_recent_courses(self):
-        """Test recent_courses method in Subdepartment model when there are not recent courses"""
-        create_new_semester(self, 2050)
-        self.assertFalse(self.subdepartment.recent_courses().exists())
-
     def test_has_current_course_true(self):
         """Test has_current_course method in
         Subdepartment model when it does have a course in the current semester
