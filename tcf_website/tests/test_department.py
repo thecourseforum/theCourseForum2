0de2579d190fcf1acdeb930fb7d5dceb44f112c5
artiehumphreys <artieh2003@gmail.com>
2024-06-16 15:59:02 -0400

    feat(department): test for fetching courses from current semester

--- tcf_website/tests/test_department.py
+++ tcf_website/tests/test_department.py
@@ -17,18 +17,29 @@ class DepartmentTestCase(TestCase):
         setup(self)
         Course.objects.all().delete()
         self.courses = []
-        for _ in range(5):
+        self.latest_semester = Semester().latest()
+        course_numbers = set()
+        for _ in range(50):
+            # Create random semester using helper method
             create_new_semester(
                 self,
                 randint(
-                    Semester().latest().year - 10, Semester().latest().year
+                    self.latest_semester.year - 10, self.latest_semester.year
                 ),
             )
+            # Generate random course numbers
+            while True:
+                course_number = randint(1000, 2000)
+                if course_number not in course_numbers:
+                    course_numbers.add(course_number)
+                    break
+
+            # Generate random courses
             course = Course.objects.create(
-                title=f"Course {randint(1, 100)}",
+                title="Intro to Programming",
                 subdepartment=self.subdepartment,
                 semester_last_taught=self.semester,
-                number=randint(1000, 2000),
+                number=course_number,
             )
             self.courses.append(course)
 
@@ -36,15 +47,23 @@ class DepartmentTestCase(TestCase):
         """Test Department model __str__ method"""
         self.assertEqual(str(self.department), "Computer Science")
 
-    def test_fetch_recent_courses(self):
+    def test_fetch_recent_courses_last_five(self):
         """Test Department fetch recent courses function"""
-        latest_semester = Semester().latest()
         recent_courses = self.department.fetch_recent_courses()
 
         expected_courses = [
             course
             for course in self.courses
-            if course.semester_last_taught.number >= latest_semester.number - 50
+            if course.semester_last_taught.number
+            >= self.latest_semester.number - 50
         ]
 
+    def test_fetch_recent_courses_current_semester(self):
+        """Test Department fetch recent courses function"""
+        recent_courses = self.department.fetch_recent_courses(0)
+        expected_courses = [
+            course
+            for course in self.courses
+            if course.semester_last_taught.number > self.latest_semester.number
+        ]
         self.assertEqual(set(recent_courses), set(expected_courses))
