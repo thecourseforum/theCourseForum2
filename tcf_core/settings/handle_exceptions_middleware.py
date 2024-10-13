3dfa5e4f64874a1671e2f730c1aa972142bf7dff
Junho Lee <junholee6a@gmail.com>
2024-10-13 18:29:02 -0400

    Add request headers in error logs

--- tcf_core/settings/handle_exceptions_middleware.py
+++ tcf_core/settings/handle_exceptions_middleware.py
@@ -4,6 +4,30 @@ import sys
 import traceback
 
 
+# Source: https://gist.github.com/defrex/6140951
+def pretty_request(request):
+    headers = ''
+    for header, value in request.META.items():
+        if not header.startswith('HTTP'):
+            continue
+        header = '-'.join([h.capitalize() for h in header[5:].lower().split('_')])
+        headers += '{}: {}\n'.format(header, value)
+
+    return (
+        '{method} HTTP/1.1\n'
+        'Content-Length: {content_length}\n'
+        'Content-Type: {content_type}\n'
+        '{headers}\n\n'
+        '{body}'
+    ).format(
+        method=request.method,
+        content_length=request.META['CONTENT_LENGTH'],
+        content_type=request.META['CONTENT_TYPE'],
+        headers=headers,
+        body=request.body,
+    )
+
+
 class HandleExceptionsMiddleware:
     """Records information about errors at any point."""
 
@@ -17,7 +41,11 @@ class HandleExceptionsMiddleware:
 
     def process_exception(self, request, exception):
         """Gets and prints out all errors to terminal for tracking"""
-        print("Error on Load")
-        print("Internal Server Error: " + request.get_full_path(), file=sys.stderr)
+        print("========= Internal server error =========", file=sys.stderr)
+        print("========== Request path ==========", file=sys.stderr)
+        print(request.get_full_path(), file=sys.stderr)
+        print("========== Request details ==========", file=sys.stderr)
+        print("Request: " + pretty_request(request), file=sys.stderr)
+        print("========== Exception ==========", file=sys.stderr)
         print(traceback.format_exc(), file=sys.stderr)
-        print(exception)
+        print("========================================", file=sys.stderr)
