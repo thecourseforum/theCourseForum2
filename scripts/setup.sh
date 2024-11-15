a4f1bcd811a257adcb746329e253bfe48c9867d9
Barrett Ruth <br.barrettruth@gmail.com>
2024-11-15 15:23:08 -0500

    feat(setup): install script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,13 +2,13 @@
 
 if [ -z "$1" ]; then
 	echo "Must specify location to clone and set up theCourseForum2 respository."
-	exit 1
+	exit
 fi
 
 exists() {
 	command -v "$1" >/dev/null 2>&1 || {
 		echo "Error: '$1' is not installed. Please install '$1'." >&2
-		exit 1
+		exit
 	}
 }
 
@@ -16,13 +16,37 @@ exists git
 
 exists docker
 
-if [ ! git clone 'git@github.com/thecourseforum/theCourseForum2.git' ]; then
+git clone git@github.com:thecourseforum/theCourseForum2.git
+if [ "$?" -ne 0 ]; then
 	echo "Unable to clone thecourseforum/theCourseForum2 with SSH. Ensure you have an ssh key set up on your GitHub account."
 	echo "See https://docs.github.com/en/authentication/connecting-to-github-with-ssh for more info."
 	exit 1
 fi
 
-docker compose up --build --no-cache
-docker exec -i tcf_db psql tcf_db -U tcf_django -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
-docker exec -i tcf_db psql tcf_db -U tcf_django -c "CREATE EXTENSION IF NOT EXISTS pg_trgm;"
-docker exec -i tcf_db psql tcf_db -U tcf_django <db/latest.sql
+cd theCourseForum2 || exit
+
+echo ""
+echo "============================================"
+echo "Please ensure that the '.env' file exists in the repository directory: $1"
+echo "Once you have created the '.env' file, press Enter to continue."
+echo "If the '.env' file does not exist, the script will exit."
+echo "============================================"
+
+printf "Press Enter to continue after verifying the '.env' file..."
+read -r _
+
+[ ! -f ".env" ] && echo "Error: '.env' file does not exist in '$1'. Follow the installation instructions in doc/dev.md after creating a '.env' file." && exit 1
+
+docker compose --no-cache
+docker compose up
+(
+	docker exec -i tcf_db psql tcf_db -U tcf_django -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
+	docker exec -i tcf_db psql tcf_db -U tcf_django -c "CREATE EXTENSION IF NOT EXISTS pg_trgm;"
+	docker exec -i tcf_db psql tcf_db -U tcf_django <db/latest.sql
+) &
+
+echo ""
+echo "============================================"
+echo "Your're good to go!"
+echo "Run the project with \`docker compose up\`"
+echo "============================================"
