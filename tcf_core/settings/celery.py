7e5055f2b2a8d7930d4a34ebd5da9a53b1bfbafb
Brandon Istfan <brandonistfan@gmail.com>
2024-11-17 05:45:13 -0500

    async scheduled enrollment availability

---
+++ tcf_core/settings/celery.py
@@ -0,0 +1,11 @@
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tcf_core.settings.base')
+
+app = Celery('tcf_core')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+print("Discovered tasks:", app.tasks.keys())
