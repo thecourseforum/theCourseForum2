c556867dcd2d923b917e7f7247d4f2694ffe0f40
Brandon Istfan <brandonistfan@gmail.com>
2025-02-02 14:51:37 -0500

    lint fixes

--- tcf_website/views/browse.py
+++ tcf_website/views/browse.py
@@ -7,7 +7,6 @@ import json
 from threading import Thread
 from typing import Any
 
-from django.core.cache import cache
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Avg, CharField, F, Q, Value
 from django.db.models.functions import Concat
@@ -281,7 +280,7 @@ def course_instructor(request, course_id, instructor_id, method="Most Recent"):
         for time in section.section_times.split(","):
             if len(time) > 0:
                 times.append(time)
-        
+
         section_enrollment = SectionEnrollment.objects.filter(section=section).first()
         enrollment_data = {
             'enrollment_taken': section_enrollment.enrollment_taken if section_enrollment else None,
@@ -289,7 +288,7 @@ def course_instructor(request, course_id, instructor_id, method="Most Recent"):
             'waitlist_taken': section_enrollment.waitlist_taken if section_enrollment else None,
             'waitlist_limit': section_enrollment.waitlist_limit if section_enrollment else None
         }
-        
+
         section_info["sections"][section.sis_section_number] = {
             "type": section.section_type,
             "units": section.units,
