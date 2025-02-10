0348ae3a6f93673459852a89a144cc2325d27a41
gthsun <sundaram.geethan@gmail.com>
2025-02-09 23:48:55 -0500

    Made input Instructor name and Course code only for average sentiments

--- tcf_website/management/commands/average_sentiments.py
+++ tcf_website/management/commands/average_sentiments.py
@@ -1,15 +1,24 @@
 import os
+import re
 import pandas as pd
+
+def extract_professor_name(professor_full):
+    return re.match(r'^[^()]+', professor_full).group().strip()
+
+def extract_course_mnemonic(course_full):
+    return course_full.split(' |')[0].strip()
 
 def avg_sentiment_df_creator():
     current_directory = os.path.dirname(os.path.abspath(__file__))
     reviews_data_path = os.path.join(current_directory, "reviews_data", "reviews_data_with_sentiment.csv")
     df = pd.read_csv(reviews_data_path)
-    avg_sentiment_df = df.groupby(["instructor", "course"])["sentiment_score"].mean().reset_index()
+    df["instructor_name_only"] = df["instructor"].apply(extract_professor_name)
+    df["course_code_only"] = df["course"].apply(extract_course_mnemonic)
+    avg_sentiment_df = df.groupby(["instructor_name_only", "course_code_only"])["sentiment_score"].mean().reset_index()
     return avg_sentiment_df
 
 def query_average(df, professor, course):
-    result = df[(df["instructor"] == professor) & (df["course"] == course)]
+    result = df[(df["instructor_name_only"] == professor) & (df["course_code_only"] == course)]
     if result.empty:
         print("No data found for the given professor and course.")
     else:
