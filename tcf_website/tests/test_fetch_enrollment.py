e3469f47d32b33796a617b62be7e49a8e3e8fdb0
Brandon Istfan <brandonistfan@gmail.com>
2025-02-17 16:29:53 -0500

    switching to already defined setup function for testing

--- tcf_website/tests/test_fetch_enrollment.py
+++ tcf_website/tests/test_fetch_enrollment.py
@@ -2,11 +2,9 @@
 
 from unittest.mock import patch
 from django.test import TestCase
-from tcf_website.models import (
-    Course, Department, School, Section,
-    SectionEnrollment, Semester, Subdepartment
-)
+from tcf_website.models import SectionEnrollment
 from tcf_website.management.commands.fetch_enrollment import fetch_section_data
+from .test_utils import setup
 
 
 class FetchEnrollmentTestCase(TestCase):
@@ -14,30 +12,9 @@ class FetchEnrollmentTestCase(TestCase):
 
     def setUp(self):
         """Set up test data."""
-        self.school = School.objects.create(name="Test School")
-        self.department = Department.objects.create(
-            name="Test Department",
-            school=self.school
-        )
-        self.subdepartment = Subdepartment.objects.create(
-            name="Test Subdepartment",
-            department=self.department
-        )
-        self.semester = Semester.objects.create(
-            number="1234",
-            year=2025
-        )
-        self.course = Course.objects.create(
-            title="Test Course",
-            number=101,
-            subdepartment=self.subdepartment,
-            semester_last_taught=self.semester
-        )
-        self.section = Section.objects.create(
-            course=self.course,
-            semester=self.semester,
-            sis_section_number="99999"
-        )
+        setup(self)
+        # pylint: disable=no-member
+        self.section = self.section_course
 
     @patch('tcf_website.management.commands.fetch_enrollment.session.get')
     def test_fetch_enrollment_success(self, mock_get):
