6d8648ed46022ec683d0dec76c56c4d432fdb3ee
angadv12 <angadbrar@outlook.com>
2025-02-21 21:28:02 -0500

    eslint style fix 5: added newlines

--- tcf_website/static/reviews/reviews.js
+++ tcf_website/static/reviews/reviews.js
@@ -1,3 +1,5 @@
+import { handleVote } from "./review";
+
 document.addEventListener("DOMContentLoaded", function () {
   const currentPage = 1;
   const courseId = document.getElementById("reviews-container").dataset.courseId;
@@ -12,31 +14,37 @@ document.addEventListener("DOMContentLoaded", function () {
         $("#reviews-container").html(data.reviews_html);
 
         // update pagination buttons
-        $(".pagination-button").off("click").on("click", function (event) {
-          event.preventDefault();
-          fetchReviews($(this).data("page"));
-        });
+        $(".pagination-button")
+          .off("click")
+          .on("click", function (event) {
+            event.preventDefault();
+            fetchReviews($(this).data("page"));
+          });
 
         // reattach voting event handlers
         attachVoteHandlers();
       },
       error: function () {
         console.error("Failed to load reviews.");
-      }
+      },
     });
   }
 
   function attachVoteHandlers() {
     $(".upvote").each(function () {
-      $(this).off("click").on("click", function () {
-        handleVote(this.id.substring(6), true);
-      });
+      $(this)
+        .off("click")
+        .on("click", function () {
+          handleVote(this.id.substring(6), true);
+        });
     });
 
     $(".downvote").each(function () {
-      $(this).off("click").on("click", function () {
-        handleVote(this.id.substring(8), false);
-      });
+      $(this)
+        .off("click")
+        .on("click", function () {
+          handleVote(this.id.substring(8), false);
+        });
     });
   }
 
