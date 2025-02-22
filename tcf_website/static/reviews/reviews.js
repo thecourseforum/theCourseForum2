e36ac315c97b417077b2a36f6fd5bb0fac55bd3e
angadv12 <angadbrar@outlook.com>
2025-02-21 21:17:16 -0500

    eslint style fix 3

--- tcf_website/static/reviews/reviews.js
+++ tcf_website/static/reviews/reviews.js
@@ -1,45 +1,48 @@
+import { handleVote } from "./review";
+
 document.addEventListener("DOMContentLoaded", function () {
-	const currentPage = 1;
-	const courseId = document.getElementById("reviews-container").dataset.courseId;
-	const instructorId = document.getElementById("reviews-container").dataset.instructorId;
+    const currentPage = 1;
+    const courseId = document.getElementById("reviews-container").dataset.courseId;
+    const instructorId = document.getElementById("reviews-container").dataset.instructorId;
 
-	function fetchReviews(page) {
-		$.ajax({
-			url: `/reviews/paginated/${courseId}/${instructorId}?page=${page}`,
-			type: "GET",
-			dataType: "json",
-			success: function (data) {
-				$("#reviews-container").html(data.reviews_html);
+    function fetchReviews(page) {
+        $.ajax({
+            url: `/reviews/paginated/${courseId}/${instructorId}?page=${page}`,
+            type: "GET",
+            dataType: "json",
+            success: function (data) {
+                $("#reviews-container").html(data.reviews_html);
 
-				// update pagination buttons
-				$(".pagination-button").off("click").on("click", function (event) {
-						event.preventDefault();
-						fetchReviews($(this).data("page"));
-				});
+                // update pagination buttons
+                $(".pagination-button").off("click").on("click", function (event) {
+                    event.preventDefault();
+                    fetchReviews($(this).data("page"));
+                });
 
-				// reattach voting event handlers
-				attachVoteHandlers();
-			},
-			error: function () {
-				console.error("Failed to load reviews.");
-			},
-		});
-	}
+                // reattach voting event handlers
+                attachVoteHandlers();
 
-	function attachVoteHandlers() {
-		$(".upvote").each(function () {
-			$(this).off("click").on("click", function () {
-				handleVote(this.id.substring(6), true);
-			});
-		});
+            },
+            error: function () {
+                console.error("Failed to load reviews.");
+            }
+        });
+    }
 
-		$(".downvote").each(function () {
-			$(this).off("click").on("click", function () {
-				handleVote(this.id.substring(8), false);
-			});
-		});
-	}
+    function attachVoteHandlers() {
+        $(".upvote").each(function () {
+            $(this).off("click").on("click", function () {
+                handleVote(this.id.substring(6), true);
+            });
+        });
 
-	// initial fetch
-	fetchReviews(currentPage);
+        $(".downvote").each(function () {
+            $(this).off("click").on("click", function () {
+                handleVote(this.id.substring(8), false);
+            });
+        });
+    }
+
+    // initial fetch
+    fetchReviews(currentPage);
 });
