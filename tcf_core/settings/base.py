93a58ed7ccf46dfcf673b6c464d0c672d78dde32
Barrett Ruth <br.barrettruth@gmail.com>
2025-03-02 17:02:20 -0500

    fix django remove staticfiles

--- tcf_core/settings/base.py
+++ tcf_core/settings/base.py
@@ -136,13 +136,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
