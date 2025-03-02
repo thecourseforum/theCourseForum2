3e1ac90dfe26011ffdad96cbf92c6d983862cb20
Barrett Ruth <br.barrettruth@gmail.com>
2025-03-02 16:48:58 -0500

    fix(s3): use the latest boto-storages api

--- tcf_core/settings/base.py
+++ tcf_core/settings/base.py
@@ -54,14 +54,17 @@ AWS_SECRET_ACCESS_KEY = env.str("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = env.str("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_REGION_NAME = env.str("AWS_S3_REGION_NAME", default="us-east-1")
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-AWS_DEFAULT_ACL = None
+AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
 
-STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-
-MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3.S3Storage",
+        "OPTIONS": {
+        },
+    },
+}
+STATICFILES_STORAGE = "storages.backends.s3.S3Storage"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
