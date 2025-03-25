c14e9ef9084494050cc117a7ff3a4fb00989a9e4
Jay-Lalwani <lalwanijay2005@gmail.com>
2025-03-24 23:07:04 -0400

    ignore pylint

--- tcf_website/api/enrollment.py
+++ tcf_website/api/enrollment.py
@@ -49,7 +49,9 @@ async def update_enrollment_data(course_id):
         return HttpResponseNotFound("Course not found.")
 
     course = await sync_to_async(Course.objects.get)(id=course_id)
-    latest_semester = await sync_to_async(lambda: Semester.latest())()
+    latest_semester = await sync_to_async(
+        lambda: Semester.latest()
+    )()  # pylint: disable=unnecessary-lambda
 
     sections_queryset = Section.objects.filter(course=course, semester=latest_semester)
     sections = await sync_to_async(list)(sections_queryset)
