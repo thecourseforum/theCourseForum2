51340f235a9141f6b27a26ef4ff9dee281f98f92
YuDavidCao <yu.cao20041208@gmail.com>
2025-04-06 14:51:21 -0400

    linting

--- tcf_website/templatetags/url_utils.py
+++ tcf_website/templatetags/url_utils.py
@@ -1,13 +1,15 @@
+"""Template tags for URL manipulation and query parameter handling."""
+
 from django import template
 
 register = template.Library()
 
 @register.filter(name='change_page')
-def change_page(request, pageNum):
-    """Change the 'pages' search parameter to pageNum while preserving other query parameters."""
+def change_page(request, page_num):
+    """Change the 'pages' search parameter to page_num while preserving other query parameters."""
     current = request.GET.copy()
     if 'pages' in current:
-        current['pages'] = pageNum
+        current['pages'] = page_num
     else:
-        current.update({'pages': pageNum})
+        current.update({'pages': page_num})
     return current.urlencode()
