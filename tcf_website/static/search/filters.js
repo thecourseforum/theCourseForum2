b15d4a00a77939436be94b9f2e46ed7c7f16cbdc
Jay-Lalwani <lalwanijay2005@gmail.com>
2025-05-02 01:24:24 -0400

    lint

--- tcf_website/static/search/filters.js
+++ tcf_website/static/search/filters.js
@@ -1,17 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(";").shift();
-  }
-
-  function setCookie(name, value, days) {
-    const date = new Date();
-    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-    const expires = `expires=${date.toUTCString()}`;
-    document.cookie = `${name}=${value};${expires};path=/`;
-  }
-
   // Determines if any filters (besides day ones) are active
   const filterInputs = document.querySelectorAll(
     '.dropdown-menu input[type="checkbox"]:not(.day-checkbox)',
