cdf8449820186c608e16063c566819686545ee18
Jay-Lalwani <lalwanijay2005@gmail.com>
2025-05-04 16:45:59 -0400

    update assistive tech labels; defend against missing inputs in mode_toggle

--- tcf_website/static/club/mode_toggle.js
+++ tcf_website/static/club/mode_toggle.js
@@ -5,6 +5,15 @@ document.addEventListener("DOMContentLoaded", function () {
   if (radioToggle) {
     const coursesRadio = document.getElementById("search-mode-courses");
     const clubsRadio = document.getElementById("search-mode-clubs");
+    
+    // Abort early if the expected inputs are missing
+    if (!coursesRadio || !clubsRadio) {
+      console.error(
+        "mode_toggle.js: Could not find expected #search-mode-(courses|clubs) radios.",
+      );
+      return;
+    }
+    
     const filterButtonElement = document.getElementById("filter-button");
 
     // If filter button exists, handle toggle state affecting filter button
@@ -12,9 +21,11 @@ document.addEventListener("DOMContentLoaded", function () {
       function updateFilterButtonState() {
         if (clubsRadio.checked) {
           filterButtonElement.disabled = true;
+          filterButtonElement.setAttribute("aria-disabled", "true");
           filterButtonElement.classList.add("disabled");
         } else {
           filterButtonElement.disabled = false;
+          filterButtonElement.setAttribute("aria-disabled", "false");
           filterButtonElement.classList.remove("disabled");
         }
       }
