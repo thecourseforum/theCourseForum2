6522a1ce31b53a590b43d77cbf4d995a817c70bd
Yogesh Thambidurai <yogesh@gyoge.com>
2025-10-19 14:34:55 -0400

    use uv for package management and build

--- scripts/container-startup.sh
+++ scripts/container-startup.sh
@@ -3,15 +3,15 @@ set -e
 
 echo 'container running'
 
-python manage.py migrate
+uv run manage.py migrate
 echo 'migrate ran'
 
-python manage.py collectstatic --noinput
+uv run manage.py collectstatic --noinput
 echo 'collectstatic ran'
 
-python manage.py invalidate_cachalot tcf_website
+uv run manage.py invalidate_cachalot tcf_website
 
-python manage.py clearsessions
+uv run manage.py clearsessions
 
 # Add custom commands here
 # python manage.py load_semester 2026_spring 
