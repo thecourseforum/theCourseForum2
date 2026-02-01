f62304f3d9be6fa72123d66b88983435d1ca1683
Himesh Ahuja <hhhimesh.131@gmail.com>
2026-02-01 14:33:50 -0500

    Finish

--- tcf_core/settings/dev.py
+++ tcf_core/settings/dev.py
@@ -4,7 +4,7 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "192.168.1.247", ".grok.io"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".grok.io"]
 
 # Local PostgreSQL database
 DATABASES = {
