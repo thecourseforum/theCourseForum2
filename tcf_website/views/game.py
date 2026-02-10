d93eb4cd5e8e0e3d99e00df09439f0eca9440ce8
ayan <ayanrasulova13@gmail.com>
2026-02-10 12:55:29 -0500

    basic fetching review skeleton

--- tcf_website/views/game.py
+++ tcf_website/views/game.py
@@ -1,8 +1,11 @@
 """Views for gamification features (game, leaderboard)"""
 
 from django import forms
+from django.core.cache import cache
 from django.core.validators import MaxValueValidator
 from django.shortcuts import render
+
+from ..models import Club, Course, Instructor, Review, Semester
 
 
 class GameForm(forms.Form):
@@ -21,8 +24,14 @@ class GameForm(forms.Form):
 
 def game(request):
     if request.method == "GET":
-        # fetch review - check doc for details
-        return render(request, "game/game.html")
+        # fetch random review - check doc for details
+
+        review = cache.get("daily_review")
+        if not review:
+            review = Review.objects.order_by("?").first()
+            cache.set("daily_review", review)
+
+        return render(request, "game/game.html", {"review": review})
     elif request.method == "POST":
         # process guess
         # use ajax to get feedback information (correctness of guesses) to display on page
