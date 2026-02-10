9d2e983fe03fdd57ceda2f8166361b1028096a95
ayan <ayanrasulova13@gmail.com>
2026-02-10 13:16:29 -0500

    updated cache to be daily

--- tcf_website/views/game.py
+++ tcf_website/views/game.py
@@ -1,5 +1,7 @@
 """Views for gamification features (game, leaderboard)"""
 
+import datetime
+
 from django import forms
 from django.core.cache import cache
 from django.core.validators import MaxValueValidator
@@ -22,16 +24,29 @@ class GameForm(forms.Form):
     )
 
 
+cache_timeout = 60 * 60 * 24  # 24 hours
+
+
+def get_daily_review():  # fetch or retrieve cached daily review, refreshes at midnight
+    now = datetime.datetime.now()
+    date = now.strftime("%Y-%m-%d")
+    cache_key = f"daily_review_{date}"  # cache key has current date
+
+    review = cache.get(cache_key)
+    if not review:
+        # if the date has changed (12:00 am) then get the new review and reset cache timer
+        review = Review.objects.order_by("?").first()
+        cache.set(cache_key, review, cache_timeout)
+
+    return review
+
+
 def game(request):
     if request.method == "GET":
         # fetch random review - check doc for details
-
-        review = cache.get("daily_review")
-        if not review:
-            review = Review.objects.order_by("?").first()
-            cache.set("daily_review", review)
-
+        review = get_daily_review()
         return render(request, "game/game.html", {"review": review})
+
     elif request.method == "POST":
         # process guess
         # use ajax to get feedback information (correctness of guesses) to display on page
