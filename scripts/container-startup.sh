5559479786cda3556d48f5bf5dabd0c68d1c6315
Jack Rhoa <jackrhoa@users.noreply.github.com>
2026-02-15 14:51:58 -0500

    modify script file

--- scripts/container-startup.sh
+++ scripts/container-startup.sh
@@ -14,7 +14,8 @@ python manage.py invalidate_cachalot tcf_website
 python manage.py clearsessions
 
 # Add custom commands here
-# python manage.py load_semester 2026_spring 
+python manage.py load_semester 2026_spring
+python manage.py load_grades ALL_DANGEROUS
 
 echo 'Starting Django Server...'
 exec gunicorn tcf_core.wsgi:application --bind 0.0.0.0:80 --log-level "info" --timeout 120
