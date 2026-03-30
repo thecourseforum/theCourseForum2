e9165ef5aac4a9aeef4d3881cdab244f75b1d7e5
Jay-Lalwani <lalwanijay2005@gmail.com>
2026-03-29 22:36:28 -0400

    feat: nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:Nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          nativeBuildInputs = [ 
+            pkgs.uv 
+            pkgs.python312
+            pkgs.nodejs_22
+          ];
+
+          UV_PYTHON_DOWNLOADS = "never";
+        };
+      });
+    };
+}
