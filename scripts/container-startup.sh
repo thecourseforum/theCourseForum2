fd646b51c7f34421868bca917ae122edfe100b26
Jack Rhoa <jackrhoa@users.noreply.github.com>
2026-03-31 13:04:50 -0400

    clear startup script

--- scripts/container-startup.sh
+++ scripts/container-startup.sh
@@ -14,7 +14,6 @@ python manage.py invalidate_cachalot tcf_website
 python manage.py clearsessions
 
 # Add custom commands here
-python manage.py load_semester 2026_summer
 
 echo 'Starting Django Server...'
 exec gunicorn tcf_core.wsgi:application --bind 0.0.0.0:80 --log-level "info" --timeout 120
