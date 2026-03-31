2bdd92f8b8e592ead3ad83d07c6f5f2b0f6157c0
Yogesh Thambidurai <yogesh@gyoge.com>
2026-03-31 17:14:12 -0400

    route s3 through cloudfront and disable public access

--- iac/s3.tf
+++ iac/s3.tf
@@ -12,10 +12,10 @@ resource "aws_s3_bucket" "static" {
 resource "aws_s3_bucket_public_access_block" "static" {
   bucket = aws_s3_bucket.static.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 # Versioning (disabled for cost optimization)
