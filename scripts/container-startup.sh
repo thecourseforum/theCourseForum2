8ed185a9bc50567da50f56b0b74adc825335f9d9
Yogesh Thambidurai <yogesh@gyoge.com>
2026-04-01 10:28:05 -0400

    change container to listen on port 8000

--- scripts/container-startup.sh
+++ scripts/container-startup.sh
@@ -5,7 +5,7 @@ echo "Starting Django Server with Gunicorn..."
 
 # Optimize workers and threads for your container's CPU allocation
 exec gunicorn tcf_core.wsgi:application \
-    --bind 0.0.0.0:80 \
+    --bind 0.0.0.0:8000 \
     --workers 3 \
     --threads 2 \
     --log-level "info" \
