e2a251dfd9bc8d61ba5425218af08aa49a808b7a
Jack Rhoa <dev@jackrhoa.com>
2026-06-30 11:50:24 -0400

    Update nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
   inputs = {
-    nixpkgs.url = "github:Nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:Nixos/nixpkgs/nixos-26.05";
   };
 
   outputs = { self, nixpkgs }:
     let
-      systems = [ "aarch64-darwin" ];
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ]; 
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
     in
     {
